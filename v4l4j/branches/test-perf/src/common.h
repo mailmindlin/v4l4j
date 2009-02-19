d80d989320291fd45f38145ca2590fc6563a297a
gilles.gigan <gilles.gigan@gmail.com>
2009-02-19 22:55:43 +0000

    Create test-perf branch from trunk

---
+++ v4l4j/branches/test-perf/src/common.h
@@ -0,0 +1,86 @@
+/*
+* Copyright (C) 2007-2008 Gilles Gigan (gilles.gigan@gmail.com)
+* eResearch Centre, James Cook University (eresearch.jcu.edu.au)
+*
+* This program was developed as part of the ARCHER project
+* (Australian Research Enabling Environment) funded by a
+* Systemic Infrastructure Initiative (SII) grant and supported by the Australian
+* Department of Innovation, Industry, Science and Research
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public  License as published by the
+* Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef H_COMMON__
+#define H_COMMON__
+
+#include <stdio.h>
+#include <jpeglib.h>
+
+#include "libv4l.h"
+
+struct v4l4j_device;
+
+struct jpeg {
+	struct jpeg_compress_struct *cinfo;
+	struct jpeg_error_mgr *jerr;
+	struct jpeg_destination_mgr *destmgr;
+	//struct v4l4j_device, void *src, void *dst
+	void (*jpeg_encode) (struct v4l4j_device *, void *, void *);
+};
+
+struct v4l4j_device {
+	struct video_device *vdev;	//the libv4l struct
+	struct jpeg *j;				//the jpeg compressor
+	unsigned char **bufs;		//the buffers holding the last JPEG compressed frame
+	int jpeg_quality;			//the jpeg quality, set to -1 if disable
+	int capture_len;			//the size of the last captured frame returned by libv4l
+	int len;					//the size of the frame after JPEG compression
+	int buf_id;					//the index of the buffer where the next frame goes
+};
+
+#define JPEG_SUPPORTED_FORMATS		{JPEG, MJPEG, YUV420, YUYV, RGB24, YVYU}
+#define NB_JPEG_SUPPORTED_FORMATS	6
+
+#define BYTEBUFER_CLASS			"java/nio/ByteBuffer"
+#define V4L4J_PACKAGE			"au/edu/jcu/v4l4j/"
+#define FRAMEGRABBER_CLASS		V4L4J_PACKAGE "FrameGrabber"
+#define CONTROL_CLASS			V4L4J_PACKAGE "Control"
+#define CONSTANTS_CLASS			V4L4J_PACKAGE "V4L4JConstants"
+#define EXCEPTION_PACKAGE		V4L4J_PACKAGE "exceptions/"
+#define GENERIC_EXCP			EXCEPTION_PACKAGE "V4L4JException"
+#define INIT_EXCP				EXCEPTION_PACKAGE "InitialisationException"
+#define DIM_EXCP				EXCEPTION_PACKAGE "ImageDimensionException"
+#define CHANNEL_EXCP			EXCEPTION_PACKAGE "CaptureChannelException"
+#define FORMAT_EXCP				EXCEPTION_PACKAGE "ImageFormatException"
+#define STD_EXCP				EXCEPTION_PACKAGE "VideoStandardException"
+#define CTRL_EXCP				EXCEPTION_PACKAGE "ControlException"
+#define RELEASE_EXCP			EXCEPTION_PACKAGE "ReleaseException"
+#define INVALID_VAL_EXCP		EXCEPTION_PACKAGE "InvalidValue"
+#define JNI_EXCP				EXCEPTION_PACKAGE "JNIException"
+
+
+/* Exception throwing helper */
+#define EXCEPTION_MSG_LENGTH	100
+#define THROW_EXCEPTION(e, c, format, ...)\
+								do {\
+									char msg[EXCEPTION_MSG_LENGTH+1];\
+									jclass JV4L4JException = (*e)->FindClass(e,c);\
+									snprintf(msg, EXCEPTION_MSG_LENGTH, format, ## __VA_ARGS__);\
+									if(JV4L4JException!=0) (*e)->ThrowNew(e, JV4L4JException, msg);\
+								} while(0)
+
+
+
+#endif /*H_COMMON_*/
---
+++ v4l4j/branches/test-perf/src/debug.h
@@ -0,0 +1,80 @@
+/*
+* Copyright (C) 2007-2008 Gilles Gigan (gilles.gigan@gmail.com)
+* eResearch Centre, James Cook University (eresearch.jcu.edu.au)
+*
+* This program was developed as part of the ARCHER project
+* (Australian Research Enabling Environment) funded by a
+* Systemic Infrastructure Initiative (SII) grant and supported by the Australian
+* Department of Innovation, Industry, Science and Research
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public  License as published by the
+* Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+#ifndef H_ALLOC_DEBUG
+#define H_ALLOC_DEBUG
+
+#define CLEAR(x) memset(&x, 0x0, sizeof(x));
+
+#define LOG_CALLS			1
+#define LOG_LIBV4L			2
+#define LOG_MEMALLOC		4
+#define LOG_JPEG			8
+#define LOG_V4L4J			16
+
+#define LOG_ALL 			( LOG_CALLS | LOG_LIBV4L | LOG_MEMALLOC | LOG_JPEG | LOG_V4L4J)
+
+#ifndef LOG_SOURCE
+#define LOG_SOURCE			LOG_ALL
+#endif
+
+#include <string.h>
+#include <stdlib.h>
+
+
+#ifdef DEBUG
+#define dprint(source, format, ...) do {if(((source) & (LOG_SOURCE))) {\
+		fprintf (stderr, "[%s:%d] " format, __FILE__, __LINE__, ## __VA_ARGS__); fflush(stderr);} } while(0)
+//		fprintf (stderr, "[%s:%d %s] " format, __FILE__, __LINE__, __PRETTY_FUNCTION__, ## __VA_ARGS__); fflush(stderr);} } while(0)
+
+#define XMALLOC(var, type, size)	\
+		do { \
+			var = (type) malloc(size); \
+			if (!var) {dprint(LOG_MEMALLOC, "[MEMALLOC]: Cant allocate %lu bytes.\n", (long unsigned int) size);} \
+			else { CLEAR(*var); \
+				dprint(LOG_MEMALLOC, "[MEMALLOC]: allocated %lu bytes of type %s for var %s (%p).\n", (long unsigned int)size, #type, #var, var);}\
+		} while (0)
+
+#define XFREE(var)					\
+		do { dprint(LOG_MEMALLOC, "[MEMALLOC]: freeing memory for var %s (%p).\n", #var, var);\
+			if (var) { free(var); } \
+			else { dprint(LOG_MEMALLOC, "[MEMALLOC]: Trying to free a NULL pointer.\n");}\
+		} while (0)
+
+#else
+#define dprint(source, format, ...)
+
+#define XMALLOC(var, type, size)	\
+		do { \
+			var = (type) malloc(size); \
+			if (!var) {fprintf(stderr,"[%s:%d %s] MEMALLOC: OUT OF MEMORY !!! Cant allocate %lu bytes.\n", __FILE__, __LINE__, __PRETTY_FUNCTION__, (long unsigned int) size); fflush(stderr);} \
+			else { CLEAR(*var);}\
+		} while (0)
+
+#define XFREE(var)					\
+		do { if (var) { free(var); } } while (0)
+
+#endif //#ifdef DEBUG
+
+#endif
