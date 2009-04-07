6a0e7ee09c1b1bce90703df76344bfe1175b05af
gilles.gigan <gilles.gigan@f47d2976-8228-11dd-9899-c7ba65f7c4c7>
2009-04-07 07:27:21 +0000

    v4l4j trunk:   * fixed svn-version.sh script to not print out error if cwd is not a WC

--- v4l4j/trunk/libvideo/svn-version.sh
+++ v4l4j/trunk/libvideo/svn-version.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-branch="$(svn info | grep 'URL')"
+branch="$(svn info 2>/dev/null | grep 'URL')" 
 branch="${branch%/*}"
 branch="${branch##*/}"
-rev="${branch}_r$(svn info | grep 'Revision' | cut -f 2 -d ' ')"
+rev="${branch}_r$(svn info 2>/dev/null | grep 'Revision' | cut -f 2 -d ' ')"
 
-if [ "x${rev}" != "x" ]; then
+if [ "x${rev}" != "x_r" ]; then
 	echo "#define VER_REV \"${rev}\"" > version.h
 else
         echo "#define VER_REV \"UNKNOWN\"" > version.h
