207b2186fe4eacc0f2b5785d308de0112ee48967
gilles.gigan <gilles.gigan@gmail.com>
2009-04-21 08:22:33 +0000

    Created devel branch

---
+++ v4l4j/branches/devel/src/au/edu/jcu/v4l4j/examples/GetFrameRate.java
@@ -0,0 +1,210 @@
+/*
+* Copyright (C) 2007-2008 Gilles Gigan (gilles.gigan@gmail.com)
+* eResearch Centre, James Cook University (eresearch.jcu.edu.au)
+*
+* This program was developed as part of the ARCHER project
+* (Australian Research Enabling Environment) funded by a   
+* Systemic Infrastructure Initiative (SII) grant and supported by the Australian
+* Department of Innovation, Industry, Science and Research
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public  License as published by the
+* Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+* or FITNESS FOR A PARTICULAR PURPOSE.  
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package au.edu.jcu.v4l4j.examples;
+
+import java.io.IOException;
+
+import au.edu.jcu.v4l4j.FrameGrabber;
+import au.edu.jcu.v4l4j.V4L4JConstants;
+import au.edu.jcu.v4l4j.VideoDevice;
+import au.edu.jcu.v4l4j.exceptions.V4L4JException;
+
+public class GetFrameRate {
+	public static void main(String[] args) throws V4L4JException, IOException {
+		String dev;
+		int w, h, std, channel, inFmt, outFmt, captureLength = 10;
+		//Check if we have the required args
+		//otherwise put sensible values in
+		try {
+			dev = args[0];
+		} catch (Exception e){
+			dev = "/dev/video0";
+		}
+		try {
+			w = Integer.parseInt(args[1]);
+		} catch (Exception e){
+			w = V4L4JConstants.MAX_WIDTH;
+		}
+		try{			
+			h = Integer.parseInt(args[2]);
+		} catch  (Exception e) {
+			h = V4L4JConstants.MAX_HEIGHT;
+		}
+		try {
+			std = Integer.parseInt(args[3]);
+		} catch (Exception e) {
+			std = 0;
+		}
+		try {
+			channel = Integer.parseInt(args[4]);
+		} catch (Exception e){
+			channel = 0;
+		}
+		try {
+			inFmt = Integer.parseInt(args[5]);
+		} catch (Exception e){
+			inFmt = -1;
+		}
+		
+		//RAW: 0 , JPEG:1, RGB:2 , bgr=3, yuv=4, yvu=5
+		try {
+			outFmt = Integer.parseInt(args[6]);
+		} catch (Exception e){
+			outFmt = 0;
+		}
+		
+
+		long start=0, now=0;
+		int n=0;
+		FrameGrabber f = null;
+		VideoDevice vd = null;
+
+		System.out.println("This program will open "+dev+", capture frames for "
+					+ captureLength+ " seconds and print the FPS");
+
+		try {
+			vd = new VideoDevice(dev);
+			if(outFmt==1 && vd.supportJPEGConversion()) {
+				if(inFmt==-1){
+					System.out.println("no capture format specified, let v4l4j find a suitable one");
+					f= vd.getJPEGFrameGrabber(w, h, channel, std, 80);
+				} else if(vd.getDeviceInfo().getFormatList().getJPEGEncodableFormat(inFmt)==null) {
+					System.out.println("Capture format "+inFmt+" doesnt exist, let v4l4j find a suitable one");
+					f= vd.getJPEGFrameGrabber(w, h, channel, std, 80);
+				}else{ 
+					System.out.println("Trying capture format "+vd.getDeviceInfo().getFormatList().getJPEGEncodableFormat(inFmt).getName());
+					f= vd.getJPEGFrameGrabber(w, h, channel, std, 80, vd.getDeviceInfo().getFormatList().getJPEGEncodableFormat(inFmt));
+				}				
+				System.out.println("Output image format: JPEG");
+			} else if(outFmt==2 && vd.supportRGBConversion()) {
+				if(inFmt==-1){
+					System.out.println("no capture format specified, let v4l4j find a suitable one");
+					f= vd.getRGBFrameGrabber(w, h, channel, std);
+				} else if(vd.getDeviceInfo().getFormatList().getRGBEncodableFormat(inFmt)==null) {
+					System.out.println("Capture format "+inFmt+" doesnt exist, trying to find a suitable one");
+					f= vd.getRGBFrameGrabber(w, h, channel, std);
+				} else { 
+					System.out.println("Trying capture format "+vd.getDeviceInfo().getFormatList().getRGBEncodableFormat(inFmt).getName());
+					f= vd.getRGBFrameGrabber(w, h, channel, std,vd.getDeviceInfo().getFormatList().getRGBEncodableFormat(inFmt));
+				}
+				System.out.println("Output image format: RGB");
+			} else if(outFmt==3 && vd.supportBGRConversion()) {
+				if(inFmt==-1){
+					System.out.println("no capture format specified, let v4l4j find a suitable one");
+					f= vd.getBGRFrameGrabber(w, h, channel, std);
+				} else if(vd.getDeviceInfo().getFormatList().getBGREncodableFormat(inFmt)==null) {
+					System.out.println("Capture format "+inFmt+" doesnt exist, trying to find a suitable one");
+					f= vd.getBGRFrameGrabber(w, h, channel, std);
+				} else { 
+					System.out.println("Trying capture format "+vd.getDeviceInfo().getFormatList().getBGREncodableFormat(inFmt).getName());
+					f= vd.getBGRFrameGrabber(w, h, channel, std,vd.getDeviceInfo().getFormatList().getBGREncodableFormat(inFmt));
+				}
+				System.out.println("Output image format: BGR");
+			} else if(outFmt==4 && vd.supportYUVConversion()) {
+				if(inFmt==-1){
+					System.out.println("no capture format specified, let v4l4j find a suitable one");
+					f= vd.getYUVFrameGrabber(w, h, channel, std);
+				} else if(vd.getDeviceInfo().getFormatList().getYUVEncodableFormat(inFmt)==null) {
+					System.out.println("Capture format "+inFmt+" doesnt exist, trying to find a suitable one");
+					f= vd.getYUVFrameGrabber(w, h, channel, std);
+				} else { 
+					System.out.println("Trying capture format "+vd.getDeviceInfo().getFormatList().getYUVEncodableFormat(inFmt).getName());
+					f= vd.getYUVFrameGrabber(w, h, channel, std,vd.getDeviceInfo().getFormatList().getYUVEncodableFormat(inFmt));
+				}
+				System.out.println("Output image format: YUV");
+			} else if(outFmt==5 && vd.supportYVUConversion()) {
+				if(inFmt==-1){
+					System.out.println("no capture format specified, let v4l4j find a suitable one");
+					f= vd.getYVUFrameGrabber(w, h, channel, std);
+				} else if(vd.getDeviceInfo().getFormatList().getYVUEncodableFormat(inFmt)==null) {
+					System.out.println("Capture format "+inFmt+" doesnt exist, trying to find a suitable one");
+					f= vd.getYVUFrameGrabber(w, h, channel, std);
+				} else { 
+					System.out.println("Trying capture format "+vd.getDeviceInfo().getFormatList().getYVUEncodableFormat(inFmt).getName());
+					f= vd.getYVUFrameGrabber(w, h, channel, std,vd.getDeviceInfo().getFormatList().getYVUEncodableFormat(inFmt));
+				}
+				System.out.println("Output image format: YVU");
+			} else {
+				if(inFmt==-1){
+					System.out.println("No capture format specified, v4l4j will pick the first one");
+					f= vd.getRawFrameGrabber(w, h, channel, std);
+				} else {
+					if(vd.getDeviceInfo().getFormatList().getNativeFormat(inFmt)==null){
+						System.out.println("The specified input format does not exist, or is not supported by the video device");
+						throw new V4L4JException("Unsupported image format");
+					}
+					System.out.println("Trying capture format "+vd.getDeviceInfo().getFormatList().getNativeFormat(inFmt).getName());
+					f= vd.getRawFrameGrabber(w, h, channel, std,vd.getDeviceInfo().getFormatList().getNativeFormat(inFmt));					
+				}
+				System.out.println("Output image format: RAW  (same as capture format)");
+			}
+			System.out.println("Capture image format: "+f.getImageFormat().getName());
+		} catch (V4L4JException e) {
+			e.printStackTrace();
+			System.out.println("Failed to instanciate the FrameGrabber ("+dev+")");
+			vd.release();
+			throw e;
+		}
+
+		try {
+			f.startCapture();
+		} catch (V4L4JException e) {
+			e.printStackTrace();
+			System.out.println("Failed to start capture");
+			vd.releaseFrameGrabber();
+			vd.release();
+			throw e;
+		}
+
+		try {
+			System.out.println("Starting test capture at "+f.getWidth()+"x"+f.getHeight()+" for "+captureLength+" seconds");
+			now=start=System.currentTimeMillis();
+			while(now<start+(captureLength*1000)){
+				f.getFrame();
+				//Uncomment the following to dump the captured frame to a jpeg file
+				//also import java.io.FileOutputStream 
+				//new FileOutputStream("file"+n+".raw").getChannel().write(f.getFrame());
+				n++;
+				now=System.currentTimeMillis();
+			}
+		} catch (V4L4JException e) {
+			e.printStackTrace();
+			System.out.println("Failed to perform test capture");
+			f.stopCapture();
+			vd.releaseFrameGrabber();
+			vd.release();
+			throw e;
+		}
+
+		System.out.println(" =====  TEST RESULTS  =====");
+		System.out.println("\tFrames captured :"+n);
+		System.out.println("\tFPS: "+((float) n/(now/1000-start/1000)));
+		System.out.println(" =====  END  RESULTS  =====");
+
+		f.stopCapture();
+		vd.releaseFrameGrabber();
+		vd.release();
+	}
+}
---
+++ v4l4j/branches/devel/src/au/edu/jcu/v4l4j/package-info.java
@@ -0,0 +1,111 @@
+/**
+ * <h1>Video4Linux4java Main Package</h1>
+ * <h2>Introduction</h2>
+ * The Video4Linux (V4L) API provides data structure & methods to access and control
+ * video input & output hardware, as well as radio tuners. The API is divided into
+ * several interfaces, including video capture, video overlay, video output & 
+ * teletext interfaces. v4l4j provides access to the video capture 
+ * interface of V4L only. v4l4j allows you to retrieve information about a video
+ * device, access its video controls & tuners and grab images from it.  
+ * <h2>Usage</h2>
+ * <h3>Video device object</h3>
+ * To use v4l4j, the first step is to create a {@link au.edu.jcu.v4l4j.VideoDevice}
+ * object, which represents a physical video input hardware. This includes webcams 
+ * and capture / tuner cards. The only pre-requisite is that your hardware must be 
+ * supported and its driver loaded. Creating a {@link au.edu.jcu.v4l4j.VideoDevice}
+ * is simple. You only need the path to a valid device file to create this object:<br>
+ * <code>
+ * VideoDevice vd = new VideoDevice("/dev/video0");
+ * </code><br>
+ * As it is the case most of the time when interacting with hardware, you must
+ * release resources and data structures when finished. All v4l4j classes follow 
+ * this rule (with few exceptions clearly noted in their java documentation),
+ * including {@link au.edu.jcu.v4l4j.VideoDevice}s:<br>
+ * <code>
+ * vd.release();</code><br>
+ * Once an object has been <code>release()</code>d, it must not be reused, and 
+ * all references to it must be discarded, by explicitly setting them to 
+ * <code>null</code> for example. Calling any <code>release()</code> method more 
+ * than once will throw a
+ *  {@link au.edu.jcu.v4l4j.exceptions.StateException StateException}.
+ * <br>
+ * {@link au.edu.jcu.v4l4j.VideoDevice} objects give you access to 4 broad 
+ * categories:
+ * <ul>
+ * <li>information about the underlying hardware through 
+ * {@link au.edu.jcu.v4l4j.DeviceInfo} objects,</li>
+ * <li>a variety of controls through {@link au.edu.jcu.v4l4j.ControlList}
+ * objects, and</li>
+ * <li>a frame capture facility using {@link au.edu.jcu.v4l4j.FrameGrabber}
+ * objects.</li>
+ * <li>an interface to control any tuners, using {@link au.edu.jcu.v4l4j.TunerList}
+ * objects.
+ * </ul>
+ * Each of these can be obtained independently from the other, by calling 
+ * the appropriate method on a {@link au.edu.jcu.v4l4j.VideoDevice} instance.
+ * <h3>Video hardware information</h3>
+ * With a valid {@link au.edu.jcu.v4l4j.VideoDevice} object, you 
+ * can retrieve information about 
+ * the underlying hardware and find out, for example how many video inputs the 
+ * device has, how many tuners are available or the set of supported video 
+ * standards. This kind of information is contained in a 
+ * {@link au.edu.jcu.v4l4j.DeviceInfo} object. You can get a reference to a 
+ * {@link au.edu.jcu.v4l4j.DeviceInfo} object for a given video device by calling
+ * {@link au.edu.jcu.v4l4j.VideoDevice#getDeviceInfo()} on the 
+ * {@link au.edu.jcu.v4l4j.VideoDevice} object. See the documentation of the
+ * {@link au.edu.jcu.v4l4j.VideoDevice} and {@link au.edu.jcu.v4l4j.DeviceInfo}
+ * classes for more information.
+ * 
+ * <h3>Video controls</h3>
+ * Video capture devices usually have a set of controls which influence 
+ * various parameters related to the video stream. Examples are brightness, 
+ * contrast, gamma & white balance. A single control is represented by
+ * {@link au.edu.jcu.v4l4j.Control} object. The set of supported controls for 
+ * a given video device hardware-dependent and is embedded into a 
+ * {@link au.edu.jcu.v4l4j.ControlList} object, which can be obtained by calling 
+ * {@link au.edu.jcu.v4l4j.VideoDevice#getControlList()}. <strong>The 
+ * {@link au.edu.jcu.v4l4j.ControlList} object and associated 
+ * {@link au.edu.jcu.v4l4j.Control}s must be released when they are no longer 
+ * needed.</strong> This is done by calling 
+ * {@link au.edu.jcu.v4l4j.VideoDevice#releaseControlList()}. Again, once released,
+ * neither the {@link au.edu.jcu.v4l4j.ControlList} nor individual 
+ * {@link au.edu.jcu.v4l4j.Control}s must be used, and any reference to them 
+ * must be discarded, by explicitly setting them to <code>null</code> for example.
+ * For more information, check the documentation of the 
+ * {@link au.edu.jcu.v4l4j.Control} and {@link au.edu.jcu.v4l4j.ControlList} classes.
+ * 
+ * <h3>Video capture</h3>
+ * Capture in v4l4j is extremely simple. v4l4j hands out captured frames in a
+ * {@link java.nio.ByteBuffer} object, either in a native format (also called 
+ * raw format) or one of the 5 convenience formats (JPEG, RGB24, BGR24, YUV420 
+ * or YVU420). Each of these 6 output formats has its own frame grabber object,
+ * which will grab frames and convert them if required. 
+ *    
+ * The set of native {@link au.edu.jcu.v4l4j.ImageFormat}s supported by a video 
+ * device can be obtained using the {@link au.edu.jcu.v4l4j.DeviceInfo} object
+ * (see paragraph "Video hardware information" above). 
+ * When capturing in a native format, v4l4j simply hands out the 
+ * captured frame without further processing. When capturing in a convenience 
+ * format, v4l4j will transparently convert the image.
+ *  
+ * Frame capture in v4l4j is done using {@link au.edu.jcu.v4l4j.FrameGrabber}
+ * objects (or one of its subclasses):
+ * <ul>
+ * <li>First, get a frame grabber object from a video device, by invoking
+ * one of the <code>getXXXFrameGrabber()</code> methods on the video device.</li>
+ * <li>Second, when ready to capture, start it by calling 
+ * {@link au.edu.jcu.v4l4j.FrameGrabber#startCapture()}.</li>
+ * <li>Get the last frame using 
+ * {@link au.edu.jcu.v4l4j.FrameGrabber#getFrame()} and do something useful with it.</li>
+ * <li>When done capturing, stop the capture with 
+ * {@link au.edu.jcu.v4l4j.FrameGrabber#stopCapture()}. If you need to start capturing 
+ * again later, just call {@link au.edu.jcu.v4l4j.FrameGrabber#startCapture()} again.</li>
+ * <li><strong>Again, when finished with the {@link au.edu.jcu.v4l4j.FrameGrabber} 
+ * object, you must release it with a call to
+ * {@link au.edu.jcu.v4l4j.VideoDevice#releaseFrameGrabber()}.
+ * </strong> Do not use the frame grabber subsequently and set any stray reference to null.</li>
+ * </ul>
+ * More information on video capture is given in the documentation of the 
+ * {@link au.edu.jcu.v4l4j.FrameGrabber} class.
+ */
+package au.edu.jcu.v4l4j;
