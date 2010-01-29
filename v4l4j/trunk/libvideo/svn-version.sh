734264c92927c8d0c3bedcafbc252659c93196c2
gilles.gigan <gilles.gigan@gmail.com>
2010-01-29 03:47:20 +0000

    v4l4j trunk: fix the svn version script

--- v4l4j/trunk/libvideo/svn-version.sh
+++ v4l4j/trunk/libvideo/svn-version.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 branch="$(svn info 2>/dev/null | grep 'URL')" 
-branch="${branch%/*}"
 branch="${branch##*/}"
-rev="${branch}_r$(svn info 2>/dev/null | grep 'Revision' | cut -f 2 -d ' ')"
+rev="$(svn info 2>/dev/null | grep 'Revision' | cut -f 2 -d ' ')"
 
-if [ "x${rev}" != "x_r" ]; then
-	echo "#define VER_REV \"${rev}\"" > version.h
+if [ "x${rev}" != "x" ]; then
+	echo "#define VER_REV \"${branch}_r${rev}\"" > version.h
 else
-        echo "#define VER_REV \"UNKNOWN\"" > version.h
+	echo "#define VER_REV \"UNKNOWN\"" > version.h
 fi
 
