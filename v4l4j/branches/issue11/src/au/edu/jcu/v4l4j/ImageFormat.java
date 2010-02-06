534a51a6d60957bf710df324cf49328ef8a7fa9f
gilles.gigan <gilles.gigan@f47d2976-8228-11dd-9899-c7ba65f7c4c7>
2010-02-06 10:43:57 +0000

    v4l4j issue11: remove debug statements

--- v4l4j/branches/issue11/src/au/edu/jcu/v4l4j/ImageFormat.java
+++ v4l4j/branches/issue11/src/au/edu/jcu/v4l4j/ImageFormat.java
@@ -81,7 +81,6 @@ public class ImageFormat {
 	ImageFormat(String n, int i, long o) {
 		name = n;
 		libvideoID = i;
-		System.out.println("### ImageFormat ctor: v4l4j_device ptr at 0x"+Long.toHexString(o)+" ###");
 		resolutions = new ResolutionInfo(i,o);
 	}
 	
