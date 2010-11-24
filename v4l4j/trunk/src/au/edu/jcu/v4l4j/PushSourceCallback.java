08db8d0c398e70dda3a545f412bd4e4ef77619db
gilles.gigan@gmail.com <gilles.gigan@gmail.com@f47d2976-8228-11dd-9899-c7ba65f7c4c7>
2010-11-24 10:36:28 +0000

    v4l4j trunk: fix javadoc

--- v4l4j/trunk/src/au/edu/jcu/v4l4j/PushSourceCallback.java
+++ v4l4j/trunk/src/au/edu/jcu/v4l4j/PushSourceCallback.java
@@ -4,11 +4,11 @@ import au.edu.jcu.v4l4j.exceptions.V4L4JException;
 
 /**
  * Objects implementing this interface act as observers of a 
- * frame grabber in push mode. They receive a notification 
- * when a new captured frame becomes available or an exception
- * has occured while capturing. By implementing this interface
+ * frame grabber in push mode. They receive notifications 
+ * as new captured frame become available, or an exception
+ * has occurred while capturing. By implementing this interface
  * and passing it to v4l4j, new frames are delivered to your 
- * application as soon as they arrive.It is extremely important
+ * application as soon as they arrive. It is extremely important
  * that you minimise the amount of code and the time spent in
  * the {@link #nextFrame(VideoFrame)} method.<br>
  * Check the {@link FrameGrabber} page to find out how to start
@@ -22,7 +22,8 @@ public interface PushSourceCallback {
 	 * During a capture, this method is called by v4l4j to provide
 	 * the latest video frame. It is important that you minimise
 	 * the amount of code and processing done in this method in 
-	 * order to maintain the appropriate frame rate. 
+	 * order to maintain the appropriate frame rate. The frame
+	 * must be recycled when no longer used.
 	 * @param frame the latest captured frame
 	 */
 	public void nextFrame(VideoFrame frame);
