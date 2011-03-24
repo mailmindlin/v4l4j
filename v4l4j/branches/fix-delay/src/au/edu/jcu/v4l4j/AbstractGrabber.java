a4c30947630f2a3090e22293adf6b03338358f12
gilles.gigan <gilles.gigan@gmail.com>
2011-03-24 10:52:34 +0000

    branch fix-delay:  * added DualViewer example app  * added equals()/hashCode() to Framerabber and DeviceInfo

--- v4l4j/branches/fix-delay/src/au/edu/jcu/v4l4j/AbstractGrabber.java
+++ v4l4j/branches/fix-delay/src/au/edu/jcu/v4l4j/AbstractGrabber.java
@@ -659,5 +659,42 @@ abstract class AbstractGrabber implements FrameGrabber {
 			temp = state;
 		}
 	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dInfo == null) ? 0 : dInfo.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof AbstractGrabber)) {
+			return false;
+		}
+		AbstractGrabber other = (AbstractGrabber) obj;
+		if (dInfo == null) {
+			if (other.dInfo != null) {
+				return false;
+			}
+		} else if (!dInfo.equals(other.dInfo)) {
+			return false;
+		}
+		return true;
+	}
+
 }
 
