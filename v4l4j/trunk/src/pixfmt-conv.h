f970eafb3fb00856918636212c1df2e9ce080fa8
gilles.gigan <gilles.gigan@gmail.com>
2012-02-14 07:40:35 +0000

    trunk: merge branch add-sse r323

--- v4l4j/trunk/src/pixfmt-conv.h
+++ v4l4j/trunk/src/pixfmt-conv.h
@@ -28,32 +28,32 @@
 #ifdef __SSSE3__
 #include <tmmintrin.h>
 #endif
-
-// Function prototype for all conversions from YUV422 to YUV422P
-//typedef void	(*convert_to_yuv422p_fn)(uint8_t* yuyv_src, uint8_t* yuv422p_dst, uint32_t width, uint32_t height);
+#ifdef __SSE2__
+#include <emmintrin.h>
+#endif
 
 /*
  * converts 'height' lines in YUV formats into Y, U, V planes.
  */
 extern inline void	convert_yuyv_to_yuv422p(uint8_t* yuyv_src, uint8_t* yuv422p_dst, uint32_t width, uint32_t height) {
-	uint32_t 	line, col;
+	uint32_t 	nb_pixels = width * height;
 	uint8_t		*y, *u, *v;
 
 	y = yuv422p_dst;
 	u = y + width * height;
 	v = u + width * height / 2;
 
-	for(line = 0; line < height; line++) {
-		for (col = 0; col < width; col += 2) {
-			*y++ = *yuyv_src++;
-			*u++ = *yuyv_src++;
-			*y++ = *yuyv_src++;
-			*v++ = *yuyv_src++;
-		}
+	while(nb_pixels > 0) {
+		*y++ = *yuyv_src++;
+		*u++ = *yuyv_src++;
+		*y++ = *yuyv_src++;
+		*v++ = *yuyv_src++;
+		nb_pixels -= 2;
 	}
 }
 
 #ifdef __SSSE3__
+/*
 static void print_m128(char *name, __m128i *reg) {
 	char*   array = (char *)reg;
 	        printf("%s:\n%4hhu %4hhu %4hhu %4hhu \t %4hhu %4hhu %4hhu %4hhu \t "
@@ -64,6 +64,13 @@ static void print_m128(char *name, __m128i *reg) {
 	                        array[12], array[13], array[14], array[15]);
 
 }
+*/
+
+/*
+ * The method does not offer any significant speed improvement compared to its
+ * non-SSE counterpart above. So I left it out for future reference.
+ */
+
 extern inline void	convert_yuyv_to_yuv422p_ssse3(uint8_t* yuyv_src, uint8_t* yuv422p_dst, uint32_t width, uint32_t height) {
 	uint32_t	nb_pixels = width * height;
 	__m128i*	src = (__m128i *) yuyv_src;
@@ -98,24 +105,14 @@ extern inline void	convert_yuyv_to_yuv422p_ssse3(uint8_t* yuyv_src, uint8_t* yuv
 
 		y_dst++;
 
-		//print_m128("yuyv in: ", src);
 		*u_dst = _mm_shuffle_epi8 (*src, u_shuff1);
-		//print_m128("u_dst: ", u_dst);
-		//print_m128("yuyv in: ", src+1);
 		temp = _mm_shuffle_epi8 (*(src + 1), u_shuff2);
-		//print_m128("temp: ", &temp);
 		*u_dst = _mm_or_si128(*u_dst, temp);
-		//print_m128("u_dst: ", u_dst);
 
-		//print_m128("yuyv in: ", (src+2));
 		temp = _mm_shuffle_epi8 (*(src + 2), u_shuff3);
-		//print_m128("u_dst: ", temp);
 		*u_dst = _mm_or_si128(*u_dst, temp);
-		//print_m128("yuyv in: ", (src+3));
 		temp = _mm_shuffle_epi8 (*(src + 3), u_shuff4);
-		//print_m128("temp: ", &temp);
 		*u_dst = _mm_or_si128(*u_dst, temp);
-		//print_m128("u_dst: ", u_dst);
 
 		u_dst++;
 
@@ -135,41 +132,101 @@ extern inline void	convert_yuyv_to_yuv422p_ssse3(uint8_t* yuyv_src, uint8_t* yuv
 		nb_pixels -= 32;
 	}
 }
+
+#endif
+
+#ifdef __SSE2__
+extern inline void	convert_yuyv_to_yuv422p_sse2(uint8_t* yuyv_src, uint8_t* yuv422p_dst, uint32_t width, uint32_t height) {
+	uint32_t	nb_pixels = width * height;
+	__m128i*	src = (__m128i *) yuyv_src;
+	__m128i*	y_dst = (__m128i *) yuv422p_dst;
+	__m128i*	u_dst = (__m128i *) ((uint8_t *)yuv422p_dst + nb_pixels);
+	__m128i*	v_dst = (__m128i *) ((uint8_t *)yuv422p_dst + nb_pixels * 3 / 2);
+
+	__m128i		scratch = {0x0LL, 0x0LL};
+	__m128i		scratch1 = {0x0LL, 0x0LL};
+	__m128i		scratch2 = {0x0LL, 0x0LL};
+	__m128i		scratch3 = {0x0LL, 0x0LL};
+
+
+	static const __m128i y_mask = {0x00FF00FF00FF00FFLL, 0x00FF00FF00FF00FFLL};
+	static const __m128i u_mask = {0x0000FF000000FF00LL, 0x0000FF000000FF00LL};
+	static const __m128i v_mask = {0xFF000000FF000000LL, 0xFF000000FF000000LL};
+
+	while(nb_pixels > 0) {
+		// Y U Y V		Y U Y V		Y U Y V		Y U Y V
+		_mm_prefetch((char *)(src+4), _MM_HINT_NTA);
+		scratch = _mm_and_si128(*src, y_mask);
+		scratch1 = _mm_and_si128(*(src + 1), y_mask);
+		*y_dst++ = _mm_packus_epi16(scratch, scratch1);
+
+		scratch2 = _mm_and_si128(*(src + 2), y_mask);
+		scratch3 = _mm_and_si128(*(src + 3), y_mask);
+		*y_dst++ = _mm_packus_epi16(scratch2, scratch3);
+
+		scratch = _mm_and_si128(*src, u_mask);
+		scratch1 = _mm_and_si128(*(src + 1), u_mask);
+		scratch2 = _mm_and_si128(*(src + 2), u_mask);
+		scratch3 = _mm_and_si128(*(src + 3), u_mask);
+		scratch = _mm_srli_epi32(scratch, 8);
+		scratch1 = _mm_srli_epi32(scratch1, 8);
+		scratch2 = _mm_srli_epi32(scratch2, 8);
+		scratch3 = _mm_srli_epi32(scratch3, 8);
+		scratch = _mm_packs_epi32(scratch, scratch1);
+		scratch2 = _mm_packs_epi32(scratch2, scratch3);
+		*u_dst++ = _mm_packus_epi16(scratch, scratch2);
+
+		scratch = _mm_and_si128(*src, v_mask);
+		scratch1 = _mm_and_si128(*(src + 1), v_mask);
+		scratch2 = _mm_and_si128(*(src + 2), v_mask);
+		scratch3 = _mm_and_si128(*(src + 3), v_mask);
+		scratch = _mm_srli_epi32(scratch, 24);
+		scratch1 = _mm_srli_epi32(scratch1, 24);
+		scratch2 = _mm_srli_epi32(scratch2, 24);
+		scratch3 = _mm_srli_epi32(scratch3, 24);
+		scratch = _mm_packs_epi32(scratch, scratch1);
+		scratch2 = _mm_packs_epi32(scratch2, scratch3);
+		*v_dst++ = _mm_packus_epi16(scratch, scratch2);
+
+
+		// 32 pixels are processed in each iterations
+		src += 4;
+		nb_pixels -= 32;
+	}
+}
 #endif
 
 extern inline void	convert_uyvy_to_yuv422p(uint8_t* uyvy_src, uint8_t* yuv422p_dst, uint32_t width, uint32_t height) {
-	uint32_t 	line, col;
+	uint32_t 	nb_pixels = width * height;
 	uint8_t		*y, *u, *v;
 
 	y = yuv422p_dst;
 	u = y + width * height;
 	v = u + width * height / 2;
 
-	for(line = 0; line < height; line++) {
-		for (col = 0; col < width; col += 2) {
-			*u++ = *uyvy_src++;
-			*y++ = *uyvy_src++;
-			*v++ = *uyvy_src++;
-			*y++ = *uyvy_src++;
-		}
+	while(nb_pixels > 0) {
+		*u++ = *uyvy_src++;
+		*y++ = *uyvy_src++;
+		*v++ = *uyvy_src++;
+		*y++ = *uyvy_src++;
+		nb_pixels -= 2;
 	}
 }
 
 extern inline void	convert_yvyu_to_yuv422p(uint8_t* yvyu_src, uint8_t* yuv422p_dst, uint32_t width, uint32_t height) {
-	uint32_t 	line, col;
+	uint32_t 	nb_pixels = width * height;
 	uint8_t		*y, *u, *v;
 
 	y = yuv422p_dst;
 	u = y + width * height;
 	v = u + width * height / 2;
 
-	for(line = 0; line < height; line++) {
-		for (col = 0; col < width; col += 2) {
-			*y++ = *yvyu_src++;
-			*v++ = *yvyu_src++;
-			*y++ = *yvyu_src++;
-			*u++ = *yvyu_src++;
-		}
+	while(nb_pixels > 0){
+		*y++ = *yvyu_src++;
+		*v++ = *yvyu_src++;
+		*y++ = *yvyu_src++;
+		*u++ = *yvyu_src++;
+		nb_pixels -= 2;
 	}
 }
 
