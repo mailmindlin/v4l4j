abe9ddef9f895a861bea87dce8ca9bf59a17e979
mailmindlin <liamttlyrox@gmail.com>
2016-02-20 17:26:55 -0500

    More bugfixes

--- libvideo/libv4lconvert/libv4lconvert-flat.c
+++ libvideo/libv4lconvert/libv4lconvert-flat.c
@@ -52,37 +52,13 @@ struct v4lconvert_encoder_series {
 	struct v4lconvert_encoder** encoders;
 }
 
-#define GENERATE_CONVERTER_SDWH_0F(id, fn, src_fmt, dst_fmt) \
-	{\
-		.id = id,\
-		.fingerprint = fingerprint_sdwh_0f,\
-		.target.cvt_sdwh_0f = fn,\
-		.src_fmt = src_fmt,\
-		.dst_fmt = dst_fmt\
-	}
-#define GENERATE_CONVERTER_SDWH_1F(id, fn, src_fmt, dst_fmt, flag1) \
-	{\
-		.id = id,\
-		.fingerprint = fingerprint_sdwh_1f,\
-		.target.cvt_sdwh_1f = fn,\
-		.src_fmt = src_fmt,\
-		.dst_fmt = dst_fmt,\
-		.flag1 = flag1,\
-		.flag2 = NULL\
-	}
+#define GENERATE_CONVERTER_SDWH_0F(id, fn, src_fmt, dst_fmt) {.id = id, .fingerprint = fingerprint_sdwh_0f,.target.cvt_sdwh_0f = fn,.src_fmt = src_fmt,.dst_fmt = dst_fmt,.flag1 = NULL,.flag2 = NULL}
 
-#define GENERATE_CONVERTER_SD_SF_2F(id, fn, src_fmt, dst_fmt, flag1, flag2) \
-	{\
-		.id = id,\
-		.fingerprint = fingerprint_sd_sf_2f,\
-		.target.cvt_sd_sf_2f = fn,\
-		.src_fmt = src_fmt,\
-		.dst_fmt = dst_fmt,\
-		.flag1 = flag1,\
-		.flag2 = flag2\
-	}
+#define GENERATE_CONVERTER_SDWH_1F(id, fn, src_fmt, dst_fmt, flag1) {.id = id,.fingerprint = fingerprint_sdwh_1f,.target.cvt_sdwh_1f = fn,.src_fmt = src_fmt,.dst_fmt = dst_fmt,.flag1 = flag1,.flag2 = NULL}
 
-#define GENERATE_CONVERTER_SDWH_1F_x2(id, fn, src_fmt_0, src_fmt_1, dst_fmt_0, dst_fmt_1, dst_fmt) \
+#define GENERATE_CONVERTER_SD_SF_2F(id, fn, src_fmt, dst_fmt, flag1, flag2) {.id = id,.fingerprint = fingerprint_sd_sf_2f,.target.cvt_sd_sf_2f = fn,.src_fmt = src_fmt,.dst_fmt = dst_fmt,.flag1 = flag1,.flag2 = flag2}
+
+#define GENERATE_CONVERTER_SDWH_1F_x2(id, fn, src_fmt_0, src_fmt_1, dst_fmt_0, dst_fmt_1) \
 	GENERATE_CONVERTER_SD_SF_2F(id + 0, fn, src_fmt_0, dst_fmt_0, 0),\
 	GENERATE_CONVERTER_SD_SF_2F(id + 1, fn, src_fmt_1, dst_fmt_1, 1)
 
@@ -93,19 +69,19 @@ struct v4lconvert_encoder_series {
 	GENERATE_CONVERTER_SD_SF_2F(id + 3, fn, src_fmt_1, dst_fmt_1, 1, 1)
 
 extern struct v4lconvert_converter v4lconvert_converters[] = {
-	GENERATE_CONVERTER_SD_SF_2F_x4(0,		v4lconvert_rgb24_to_yuv420,	RGB32,	BGR32,	YUV420,	YVU420),
-	GENERATE_CONVERTER_SDWH_1F_x2(4,		v4lconvert_yuv420_to_rgb24,	YUV420,	YVU420,	RGB24,	RGB24),
-	GENERAGE_CONVERTER_SDWH_1F_x2(6,		v4lconvert_yuv420_to_bgr24,	YUV420,	YVU420,	BGR24,	BGR24),
+	GENERATE_CONVERTER_SD_SF_2F_x4(0,	v4lconvert_rgb24_to_yuv420,	RGB32,	BGR32,	YUV420,	YVU420),
+	GENERATE_CONVERTER_SDWH_1F_x2(4,	v4lconvert_yuv420_to_rgb24,	YUV420,	YVU420,	RGB24,	RGB24),
+	GENERAGE_CONVERTER_SDWH_1F_x2(6,	v4lconvert_yuv420_to_bgr24,	YUV420,	YVU420,	BGR24,	BGR24),
 	GENERAGE_CONVERTER_SDWH_0F(8,		v4lconvert_yuyv_to_rgb24,	YUYV,	RGB24),
 	GENERATE_CONVERTER_SDWH_0F(9,		v4lconvert_yuyv_to_bgr24,	YUYV,	BGR24),
 	GENERAGE_CONVERTER_SDWH_1F_x2(10,	v4lconvert_yuyv_to_yuv420,	YUYV,	YUYV,	YUV420,	YVU420),
-	GENERATE_CONVERTER_SDWH_0F(12,	v4lconvert_yvyu_to_rgb24,	YUYV,	RGB24),
-	GENERATE_CONVERTER_SDWH_0F(13,	v4lconvert_yvyu_to_bgr24,	YUYV,	BGR24),
-	GENERATE_CONVERTER_SDWH_0F(14,	v4lconvert_uyvy_to_rgb24,	UYVY,	RGB24),
-	GENERAGE_CONVERTER_SDWH_0F(15,	v4lconvert_uyvy_to_bgr24,	UYVY,	BGR24),
+	GENERATE_CONVERTER_SDWH_0F(12,		v4lconvert_yvyu_to_rgb24,	YUYV,	RGB24),
+	GENERATE_CONVERTER_SDWH_0F(13,		v4lconvert_yvyu_to_bgr24,	YUYV,	BGR24),
+	GENERATE_CONVERTER_SDWH_0F(14,		v4lconvert_uyvy_to_rgb24,	UYVY,	RGB24),
+	GENERAGE_CONVERTER_SDWH_0F(15,		v4lconvert_uyvy_to_bgr24,	UYVY,	BGR24),
 	GENERAGE_CONVERTER_SDWH_1F_x2(16,	v4lconvert_uyvy_to_yuv420,	UYVY,	UYVY,	YUV420,	YVU420),
-	GENERAGE_CONVERTER_SDWH_0f(17,	v4lconvert_swap_rgb,		RGB24,	BGR24),
-	GENERAGE_CONVERTER_SDWH_0f(18,	v4lconvert_swap_rgb,		BGR24,	RGB24)
+	GENERAGE_CONVERTER_SDWH_0f(17,		v4lconvert_swap_rgb,		RGB24,	BGR24),
+	GENERAGE_CONVERTER_SDWH_0f(18,		v4lconvert_swap_rgb,		BGR24,	RGB24)
 	//TODO add other converters
 };
 
