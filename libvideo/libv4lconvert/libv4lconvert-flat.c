742911ab9f7fe4ab8a42d7058ae853ae9b8fbdec
mailmindlin <liamttlyrox@gmail.com>
2016-02-20 23:10:19 -0500

    (mandatory typo fix)

--- libvideo/libv4lconvert/libv4lconvert-flat.c
+++ libvideo/libv4lconvert/libv4lconvert-flat.c
@@ -53,6 +53,8 @@ struct v4lconvert_encoder_series {
 
 #define GENERATE_CONVERTER_SDWH_1F(id, fn, src_fmt, dst_fmt, flag1) {.id = (id), .fingerprint = fingerprint_sdwh_1f, .target.cvt_sdwh_1f = (fn), .src_fmt = (src_fmt), .dst_fmt = (dst_fmt), .flag1 = flag1, .flag2 = NULL}
 
+#define GENERATE_CONVERTER_SD_SF_1F(id, fn, src_fmt, dst_fmt, flag1) {.id = id, .fingerprint = fingerprint_sd_sf_1f, .target.cvt_sd_sf_2f = (fn), .src_fmt = (src_fmt), .dst_fmt = (dst_fmt), .flag1 = flag1, .flag2 = NULL}
+
 #define GENERATE_CONVERTER_SD_SF_2F(id, fn, src_fmt, dst_fmt, flag1, flag2) {.id = id, .fingerprint = fingerprint_sd_sf_2f, .target.cvt_sd_sf_2f = (fn), .src_fmt = (src_fmt), .dst_fmt = (dst_fmt), .flag1 = flag1, .flag2 = flag2}
 
 #define GENERATE_CONVERTER_SDWH_1F_x2(id, fn, src_fmt_0, src_fmt_1, dst_fmt_0, dst_fmt_1) \
