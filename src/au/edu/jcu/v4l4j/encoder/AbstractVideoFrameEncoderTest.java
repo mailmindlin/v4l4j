762f2db5e22370cfcf02a18eba6566e0d2b3318f
mailmindlin <liamttlyrox@gmail.com>
2016-02-21 15:34:33 -0500

    Fixed NPE

--- src/au/edu/jcu/v4l4j/encoder/AbstractVideoFrameEncoderTest.java
+++ src/au/edu/jcu/v4l4j/encoder/AbstractVideoFrameEncoderTest.java
@@ -1,11 +1,10 @@
 package au.edu.jcu.v4l4j.encoder;
 
-import static org.junit.Assert.*;
-
 import java.util.Arrays;
 
 import org.junit.Test;
 
+import au.edu.jcu.v4l4j.ImagePalette;
 import junit.framework.Assert;
 
 /**
@@ -15,9 +14,10 @@ import junit.framework.Assert;
 public class AbstractVideoFrameEncoderTest {
 
 	@Test
-	public void test() {
+	public void test() throws Exception {
 		try {
-			AbstractVideoFrameEncoder encoder = new AbstractVideoFrameEncoder(5, 7, null, null);
+			
+			AbstractVideoFrameEncoder encoder = new AbstractVideoFrameEncoder(5, 7, ImagePalette.RGB32, ImagePalette.YUV420);
 			
 			int numConverters = -encoder.getConverterIds(encoder.object, null);
 			Assert.assertEquals(numConverters, 1);
@@ -27,7 +27,7 @@ public class AbstractVideoFrameEncoderTest {
 			System.out.println(Arrays.toString(converterIds));
 			Assert.assertEquals(numConverters, 1);
 			
-			encoder.release();
+			encoder.close();
 	//		fail("Not yet implemented");
 		} catch (Exception e) {
 			e.printStackTrace();
