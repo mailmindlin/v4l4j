0ec482e35608ee93b4a24c762b1fa7e0eea7371f
mailmindlin <liamttlyrox@gmail.com>
2016-02-25 19:45:58 -0500

    More testing

--- src/au/edu/jcu/v4l4j/encoder/h264/H264Test.java
+++ src/au/edu/jcu/v4l4j/encoder/h264/H264Test.java
@@ -14,25 +14,27 @@ public class H264Test {
 		assertEquals(picture.getCsp(), X264.X264_CSP_RGB);
 		assertEquals(picture.getWidth(), 600);
 		assertEquals(picture.getHeight(), 800);
-		
+
 		picture.close();
 	}
 
 	@Test
 	public void testParameters() {
-		try (H264Parameters params = new H264Parameters()) {
-			params.initDefault();
-//			params.initWithPreset(H264Parameters.PRESET_ULTRA_FAST, H264Parameters.TUNE_ZERO_LATENCY);
-			
-			params.setCsp(X264.X264_CSP_RGB);
-			params.setInputDimension(600, 800);
-			
-			params.applyFastFirstPass();
-			
-			assertEquals(params.getWidth(), 600);
-			assertEquals(params.getHeight(), 800);
-			
-			assertEquals(params.getCsp(), X264.X264_CSP_RGB);
-		}
+		H264Parameters params = new H264Parameters();
+		params.initDefault();
+		// params.initWithPreset(H264Parameters.PRESET_ULTRA_FAST,
+		// H264Parameters.TUNE_ZERO_LATENCY);
+
+		params.setCsp(X264.X264_CSP_RGB);
+		params.setInputDimension(600, 800);
+
+		params.applyFastFirstPass();
+
+		assertEquals(params.getWidth(), 600);
+		assertEquals(params.getHeight(), 800);
+
+		assertEquals(params.getCsp(), X264.X264_CSP_RGB);
+		params.close();
+
 	}
 }
