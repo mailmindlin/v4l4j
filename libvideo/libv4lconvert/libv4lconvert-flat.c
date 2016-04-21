552840e8243e827df1492efce8e323bbed3b8469
mailmindlin <liamttlyrox@gmail.com>
2016-04-21 19:19:15 -0400

    Added more conversion methods

--- libvideo/libv4lconvert/libv4lconvert-flat.c
+++ libvideo/libv4lconvert/libv4lconvert-flat.c
@@ -21,7 +21,9 @@ extern "C" {
 
 #define GENERATE_CONVERTER_SDWH_2F(id, fn, src_fmt, dst_fmt, flag1, flag2) {(id), v4lconvert_conversion_fingerprint_sdwh_1f, {.cvt_sdwh_1f = (fn)}, (src_fmt), (dst_fmt), (flag1), (flag2)}
 
-#define GENERATE_CONVERTER_SD_SF_1F(id, fn, src_fmt, dst_fmt, flag1) {(id), v4lconvert_conversion_fingerprint_sd_sf_1f, {.cvt_sd_sf_2f = (fn)}, (src_fmt), (dst_fmt), (flag1), N_A}
+#define GENERATE_CONVERTER_SD_SF_0F(id, fn, src_fmt, dst_fmt) {(id), v4lconvert_conversion_fingerprint_sd_sf_0f, {.cvt_sd_sf_0f = (fn)}, (src_fmt), (dst_fmt), N_A, N_A}
+
+#define GENERATE_CONVERTER_SD_SF_1F(id, fn, src_fmt, dst_fmt, flag1) {(id), v4lconvert_conversion_fingerprint_sd_sf_1f, {.cvt_sd_sf_1f = (fn)}, (src_fmt), (dst_fmt), (flag1), N_A}
 
 #define GENERATE_CONVERTER_SD_SF_2F(id, fn, src_fmt, dst_fmt, flag1, flag2) {(id), v4lconvert_conversion_fingerprint_sd_sf_2f, {.cvt_sd_sf_2f = (fn)}, (src_fmt), (dst_fmt), (flag1), (flag2)}
 
@@ -53,7 +55,58 @@ v4lconvert_converter_t v4lconvert_converters[NUM_CONVERTERS] = {
 	GENERATE_CONVERTER_SDWH_0F(15, v4lconvert_uyvy_to_bgr24, UYVY, BGR24),
 	GENERATE_CONVERTER_SDWH_1F_x2(16, v4lconvert_uyvy_to_yuv420, UYVY, UYVY, YUV420, YVU420),
 	GENERATE_CONVERTER_SDWH_0F(18, v4lconvert_swap_rgb, RGB24, BGR24),
-	GENERATE_CONVERTER_SDWH_0F(19, v4lconvert_swap_rgb, BGR24, RGB24)
+	GENERATE_CONVERTER_SDWH_0F(19, v4lconvert_swap_rgb, BGR24, RGB24),
+	GENERATE_CONVERTER_SD_SF_0F(20, v4lconvert_swap_uv, YUV420, YVU420),//TODO figure out if this is right...
+	GENERATE_CONVERTER_SDWH_0F(21, v4lconvert_grey_to_rgb24, GREY, RGB24),
+	GENERATE_CONVERTER_SD_SF_0F(22, v4lconvert_grey_to_yuv420, GREY, YUV420),
+	//v4lconvert_y10b_to_rgb24(struct v4lconvert_data *data, const u8 *src, u8 *dest, u32 width, u32 height);
+	//v4lconvert_y10b_to_yuv420(struct v4lconvert_data *data, const u8 *src, u8 *dest, u32 width, u32 height);
+	//v4lconvert_rgb565_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height);
+	GENERATE_CONVERTER_SDWH_0F(23, v4lconvert_rgb565_to_bgr24, RGB565, BGR24),
+	GENERATE_CONVERTER_SD_SF_1F_x2(24, v4lconvert_rgb565_to_yuv420, RGB565, RGB565, YUV420, YVU420),
+	GENERATE_CONVERTER_SDWH_1F_x2(26, v4lconvert_spca501_to_yuv420, SPCA501, SPCA501, YUV420, YVU420),
+	//v4lconvert_spca505_to_yuv420(const u8 *src, u8 *dst, u32 width, u32 height, int yvu);
+	//v4lconvert_spca508_to_yuv420(const u8 *src, u8 *dst, u32 width, u32 height, int yvu);
+	//v4lconvert_cit_yyvyuy_to_yuv420(const u8 *src, u8 *ydest, u32 width, u32 height, int yvu);
+	//v4lconvert_konica_yuv420_to_yuv420(const u8 *src, u8 *ydest, u32 width, u32 height, int yvu);
+	//v4lconvert_m420_to_yuv420(const u8 *src, u8 *ydest, u32 width, u32 height, int yvu);
+	//int v4lconvert_cpia1_to_yuv420(struct v4lconvert_data *data, const u8 *src, int src_size, u8 *dst, u32 width, u32 height, int yvu);
+	//v4lconvert_sn9c20x_to_yuv420(const u8 *src, u8 *dst, u32 width, u32 height, int yvu);
+	//int v4lconvert_se401_to_rgb24(struct v4lconvert_data *data, const u8 *src, int src_size, u8 *dest, u32 width, u32 height);
+
+int v4lconvert_decode_jpeg_tinyjpeg(struct v4lconvert_data *data, u8 *src, int src_size, u8 *dest, struct v4l2_format *fmt, unsigned int dest_pix_fmt, int flags);
+
+int v4lconvert_decode_jpeg_libjpeg(struct v4lconvert_data *data, u8 *src, int src_size, u8 *dest, struct v4l2_format *fmt, unsigned int dest_pix_fmt);
+
+int v4lconvert_decode_jpgl(const u8 *src, u32 src_size, unsigned int dest_pix_fmt, u8 *dest, u32 width, u32 height);
+
+	//v4lconvert_decode_spca561(const u8 *src, u8 *dst, u32 width, u32 height);
+
+	//v4lconvert_decode_sn9c10x(const u8 *src, u8 *dst, u32 width, u32 height);
+
+int v4lconvert_decode_pac207(struct v4lconvert_data *data, const u8 *inp, int src_size, u8 *outp, u32 width, u32 height);
+
+int v4lconvert_decode_mr97310a(struct v4lconvert_data *data, const u8 *src, int src_size, u8 *dst, u32 width, u32 height);
+
+int v4lconvert_decode_jl2005bcd(struct v4lconvert_data *data, const u8 *src, int src_size, u8 *dest, u32 width, u32 height);
+
+	//v4lconvert_decode_sn9c2028(const u8 *src, u8 *dst, u32 width, u32 height);
+
+	//v4lconvert_decode_sq905c(const u8 *src, u8 *dst, u32 width, u32 height);
+
+	//v4lconvert_decode_stv0680(const u8 *src, u8 *dst, u32 width, u32 height);
+
+	//v4lconvert_bayer_to_rgb24(const u8 *bayer, u8 *rgb, u32 width, u32 height, unsigned int pixfmt);
+
+	//v4lconvert_bayer_to_bgr24(const u8 *bayer, u8 *rgb, u32 width, u32 height, unsigned int pixfmt);
+
+	//v4lconvert_bayer_to_yuv420(const u8 *bayer, u8 *yuv, u32 width, u32 height, unsigned int src_pixfmt, int yvu);
+
+	//v4lconvert_hm12_to_rgb24(const u8 *src, u8 *dst, u32 width, u32 height);
+
+	//v4lconvert_hm12_to_bgr24(const u8 *src, u8 *dst, u32 width, u32 height);
+
+	//v4lconvert_hm12_to_yuv420(const u8 *src, u8 *dst, u32 width, u32 height, int yvu);
 	//TODO add other converters
 };
 
@@ -97,6 +150,9 @@ v4lconvert_converter_t* v4lconvert_converter_getConverterById(unsigned int conve
 	return &(v4lconvert_converters[converterId]);
 }
 
+/**
+ * 
+ */
 unsigned int v4lconvert_converter_lookupConverterByConversion(unsigned int from, unsigned int to) {
 	int i;
 	v4lconvert_converter_t* converter;
@@ -107,10 +163,11 @@ unsigned int v4lconvert_converter_lookupConverterByConversion(unsigned int from,
 		if ((converter->src_fmt == from) && (converter->dst_fmt == to))
 			return i;
 	}
-	return N_A;
+	return -1;
 }
 /**
  * Find a converter that does the conversion that you want
+ * @return the converter requested, or NULL
  */
 v4lconvert_converter_t* v4lconvert_converter_getConverterByConversion(unsigned int from, unsigned int to) {
 	int i;
@@ -122,7 +179,7 @@ v4lconvert_converter_t* v4lconvert_converter_getConverterByConversion(unsigned i
 		if ((converter->src_fmt == from) && (converter->dst_fmt == to))
 			return converter;
 	}
-	return N_A;
+	return NULL;
 }
 
 #ifdef __cplusplus
--- libvideo/libv4lconvert/libv4lconvert-flat.h
+++ libvideo/libv4lconvert/libv4lconvert-flat.h
@@ -9,6 +9,13 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+#ifndef LIBV4L_PUBLIC
+#if __GNUC__ >= 4
+#define LIBV4L_PUBLIC __attribute__ ((visibility("default")))
+#else
+#define LIBV4L_PUBLIC
+#endif
+#endif
 
 enum v4lconvert_conversion_fingerprint {
 	/**
@@ -21,6 +28,7 @@ enum v4lconvert_conversion_fingerprint {
 	v4lconvert_conversion_fingerprint_sdwh_0f,
 	v4lconvert_conversion_fingerprint_sdwh_1f,
 	v4lconvert_conversion_fingerprint_sdwh_2f,
+	v4lconvert_conversion_fingerprint_sd_sf_0f,
 	v4lconvert_conversion_fingerprint_sd_sf_1f,
 	v4lconvert_conversion_fingerprint_sd_sf_2f
 };
@@ -28,6 +36,7 @@ union v4lconvert_conversion_fn {
 	void (*cvt_sdwh_0f) (const u8* src, u8* dst, u32 width, u32 height);
 	void (*cvt_sdwh_1f) (const u8* src, u8* dst, u32 width, u32 height, int flag1);
 	void (*cvt_sdwh_2f) (const u8* src, u8* dst, u32 width, u32 height, int flag1, int flag2);
+	void (*cvt_sd_sf_0f) (const u8* src, u8* dst, const struct v4l2_format* src_fmt;
 	void (*cvt_sd_sf_1f) (const u8* src, u8* dst, const struct v4l2_format* src_fmt, int flag1);
 	void (*cvt_sd_sf_2f) (const u8* src, u8* dst, const struct v4l2_format* src_fmt, int flag1, int flag2);
 };
@@ -61,11 +70,16 @@ struct v4lconvert_encoder_series {
 	struct v4lconvert_encoder** encoders;
 };
 
+void v4lconvert_encoder_init(struct v4lconvert_encoder* encoder, u32 converterId, u32 width, u32 height);
 void v4lconvert_encoder_doConvert(struct v4lconvert_encoder* self, const u8* src, u8* dst);
-void v4lconvert_encoder_init(struct v4lconvert_encoder* encoder, unsigned int converterId, unsigned int width, unsigned int height);
-v4lconvert_converter_t* v4lconvert_converter_getConverterById(unsigned int converterId);
-unsigned int v4lconvert_converter_lookupConverterByConversion(unsigned int from, unsigned int to);
-v4lconvert_converter_t* v4lconvert_converter_getConverterByConversion(unsigned int from, unsigned int to);
+
+void v4lconvert_encoder_series_init(struct v4lconvert_encoder_series* self, u32 width, u32 height, u32 numConverters, u32* converterIds);
+int v4lconvert_encoder_series_computeConversion(struct v4lconvert_encoder_series* self, u32 width, u32 height, u32 from, u32 to);
+void v4lconvert_encoder_series_doConvert(struct v4lconvert_encoder_series* self, const u8* src, const u8* dst);
+
+v4lconvert_converter_t* v4lconvert_converter_getConverterById(u32 converterId);
+unsigned int v4lconvert_converter_lookupConverterByConversion(u32 from, u32 to);
+v4lconvert_converter_t* v4lconvert_converter_getConverterByConversion(u32 from, u32 to);
 
 #ifdef __cplusplus
 }
