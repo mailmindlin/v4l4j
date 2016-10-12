5778f852abff264a07abb7caf71dd6591fca1ee9
mailmindlin <liamttlyrox@gmail.com>
2016-10-12 01:04:18 -0400

    Fixed some typos

--- src/jniutils.c
+++ src/jniutils.c
@@ -6,12 +6,12 @@
 inline jmethodID lookupAddMethod(JNIEnv *env, jobject list) {
 	jclass listClass = (*env)->GetObjectClass(env, list);
 	if (listClass == NULL) {
-		THROW_EXCEPTION(e, JNI_EXCP, "Error looking up list class");
+		THROW_EXCEPTION(env, JNI_EXCP, "Error looking up list class");
 		return NULL;
 	}
 	jmethodID addMethod = (*env)->GetMethodID(env, listClass, "add", "(Ljava/lang/Object;)Z");
 	if (addMethod == NULL)
-		THROW_EXCEPTION(e, JNI_EXCP, "Error looking up add method of list");
+		THROW_EXCEPTION(env, JNI_EXCP, "Error looking up add method of list");
 	return addMethod;
 }
 
