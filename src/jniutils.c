899d5f48ede90cb7c78aa3e503de0b1f8a1d6a61
mailmindlin <liamttlyrox@gmail.com>
2016-11-09 17:33:07 -0500

    Fixed memory leak, added method for buffers
    It's not a real memory leak, but rather I fogot to close a reference.

--- src/jniutils.c
+++ src/jniutils.c
@@ -6,11 +6,6 @@
 #include "common.h"
 #include "debug.h"
 
-inline jmethodID lookupAddMethod(JNIEnv *env, jobject list) __attribute__ ((pure));
-inline jclass lookupClassSafe(JNIEnv *env, jobject obj) __attribute__ ((pure));
-inline int getBufferPosition(JNIEnv *env, jobject buffer) __attribute__ ((pure));
-inline int getBufferLimit(JNIEnv *env, jobject limit) __attribute__ ((pure));
-
 inline jmethodID lookupAddMethod(JNIEnv *env, jobject list) {
 	jclass listClass = (*env)->GetObjectClass(env, list);
 	if (listClass == NULL) {
@@ -36,6 +31,67 @@ inline jclass lookupClassSafe(JNIEnv *env, jobject obj) {
 }
 
 /**
+ * Get a pointer from a buffer, whether direct or not.
+ * @param env the JNI environment for this thread
+ * @param buffer The buffer to get a pointer to
+ * @param arrayRef If not null, will be set to NULL if a direct pointer can be recieved, else the array that is backing the returned pointer
+ * @param len If not null, will be set to the length of the returned pointer, in bytes.
+ */
+inline unsigned char* getBufferPointer(JNIEnv *env, jobject buffer, jbyteArray* arrayRef, unsigned int* len) {
+	unsigned char* result = (*env)->GetDirectBufferAddress(env, buffer);
+	if (result) {
+		if (arrayRef)
+			*arrayRef = NULL;
+		if (len)
+			*len = (*env)->GetDirectBufferCapacity(env, buffer);
+		return result;
+	}
+	if ((*env)->ExceptionCheck(env))
+		(*env)->ExceptionDescribe(env);
+	if (buffer == NULL) {
+		THROW_EXCEPTION(env, NULL_EXCP, "Buffer was null");
+		return NULL;
+	}
+	jclass bufferClass = (*env)->GetObjectClass(env, buffer);
+	if (!bufferClass) {
+		THROW_EXCEPTION(env, JNI_EXCP, "Unable to get buffer class");
+		return NULL;
+	}
+	jmethodID bufferHasArrayMID = (*env)->GetMethodID(env, bufferClass, "hasArray", "()Z");
+	if (!bufferHasArrayMID) {
+		(*env)->DeleteLocalRef(env, bufferClass);
+		THROW_EXCEPTION(env, JNI_EXCP, "Unable to get MID for boolean Buffer#hasArray()");
+		return NULL;
+	}
+	jboolean hasArray = (*env)->CallBooleanMethod(env, buffer, bufferHasArrayMID);
+	if ((*env)->ExceptionCheck(env) || !hasArray) {
+		(*env)->DeleteLocalRef(env, bufferClass);
+		return NULL;
+	}
+	jmethodID bufferGetArrayMID = (*env)->GetMethodID(env, bufferClass, "array", "()[B");
+	(*env)->DeleteLocalRef(env, bufferClass);
+	if (!bufferGetArrayMID) {
+		THROW_EXCEPTION(env, JNI_EXCP, "Unable to get MID for Object Buffer#array()");
+		return NULL;
+	}
+	jbyteArray array = (jbyteArray) (*env)->CallObjectMethod(env, buffer, bufferGetArrayMID);
+	if (!array) {
+		THROW_EXCEPTION(env, JNI_EXCP, "Unable to get array from Buffer");
+		return NULL;
+	}
+	jbyte* arrayElements = (*env)->GetByteArrayElements(env, array, NULL);
+	if (!arrayElements) {
+		(*env)->DeleteLocalRef(env, array);
+		THROW_EXCEPTION(env, JNI_EXCP, "Unable to get elements from array");
+		return NULL;
+	}
+	if (len)
+		*len = (*env)->GetArrayLength(env, array);
+	if (arrayRef)
+		*arrayRef = array;
+	return (unsigned char*) arrayElements;
+}
+/**
  * Set the position value for a ByteBuffer. Note that this method will create <4
  * @param env The JNI environment pointer
  * @param buffer The buffer object to set the position on
@@ -50,7 +106,9 @@ inline void setBufferPosition(JNIEnv *env, jobject buffer, int position) {
 		THROW_EXCEPTION(env, JNI_EXCP, "Could not lookup ByteBuffer#position(I) for buffer.");
 		return;
 	}
-	(*env)->CallObjectMethod(env, buffer, positionSetMethodID, position);
+	jobject result = (*env)->CallObjectMethod(env, buffer, positionSetMethodID, position);
+	if (result)
+		(*env)->DeleteLocalRef(env, result);
 }
 
 inline int getBufferPosition(JNIEnv *env, jobject buffer) {
