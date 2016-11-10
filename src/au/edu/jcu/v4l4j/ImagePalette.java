3a2bb341b7a24fdd8957d94d2abc64f2c14e0190
mailmindlin <liamttlyrox@gmail.com>
2016-11-09 22:09:36 -0500

    Doccumented palettes
    There are a lot of them, and I don't understand them all.

--- src/au/edu/jcu/v4l4j/ImagePalette.java
+++ src/au/edu/jcu/v4l4j/ImagePalette.java
@@ -2,24 +2,124 @@ package au.edu.jcu.v4l4j;
 
 import static au.edu.jcu.v4l4j.V4L4JConstants.*;
 
+/**
+ * Enum of image format palettes. Matches (or *should* match) the enum in
+ * <code>libvideo.h</code>.
+ * <p>
+ * See <a href=
+ * "https://linuxtv.org/downloads/v4l-dvb-apis/uapi/v4l/pixfmt.html">linuxtv.org/downloads/v4l-dvb-apis/uapi/v4l/pixfmt.html</a>
+ * for help.
+ * </p>
+ * 
+ * @author mailmindlin
+ */
 public enum ImagePalette {
 	// RGB formats
+	/**
+	 * RGB that fits in a byte:
+	 * <ul>
+	 * <li><strong>3</strong> bits red</li>
+	 * <li><strong>3</strong> bits green</li>
+	 * <li><strong>2</strong> bits blue</li>
+	 * </ul>
+	 * @see <a href="http://unwind.se/bgr233/">unwind.se/bgr233</a>
+	 */
 	RGB332(8, IMF_RGB332),
+	/**
+	 * RGB that fits in two bytes
+	 */
 	RGB444(16, IMF_RGB444),
+	/**
+	 * <ul>
+	 * <li><strong>1</strong> bit unused</li>
+	 * <li><strong>5</strong> bits red</li>
+	 * <li><strong>5</strong> bits green</li>
+	 * <li><strong>5</strong> bits blue</li>
+	 * </ul>
+	 */
 	RGB555(16, IMF_RGB555),
+	/**
+	 * <ul>
+	 * <li><strong>5</strong> bits red</li>
+	 * <li><strong>6</strong> bits green</li>
+	 * <li><strong>5</strong> bits blue</li>
+	 * </ul>
+	 */
 	RGB565(16, IMF_RGB565),
+	/**
+	 * Not really sure. Let me know if you figure what
+	 * the difference between this and {@link #RGB555} is.
+	 */
 	RGB555X(16, IMF_RGB555X),
+	/**
+	 * Not really sure.
+	 */
 	RGB565X(16, IMF_RGB565X),
+	/**
+	 * BGR that fits in 3 bytes:
+	 * <ul>
+	 * <li><strong>8</strong> bits blue</li>
+	 * <li><strong>8</strong> bits green</li>
+	 * <li><strong>8</strong> bits red</li>
+	 * </ul>
+	 * @see #RGB24
+	 */
 	BGR24(24, IMF_BGR24),
+	/**
+	 * RGB that fits in 3 bytes:
+	 * <ul>
+	 * <li><strong>3</strong> bits red</li>
+	 * <li><strong>3</strong> bits green</li>
+	 * <li><strong>2</strong> bits blue</li>
+	 * </ul>
+	 * @see #BGR24
+	 */
 	RGB24(24, IMF_RGB24),
+	/**
+	 * BGR that fits in 4 bytes (contains alpha channel):
+	 * <ul>
+	 * <li><strong>8</strong> bits blue</li>
+	 * <li><strong>8</strong> bits green</li>
+	 * <li><strong>8</strong> bits red</li>
+	 * <li><strong>8</strong> bits alpha</li>
+	 * </ul>
+	 * TODO make sure that the alpha channel is in the right place
+	 * @see #RGB32
+	 */
 	BGR32(32, IMF_BGR32),
+	/**
+	 * RGB that fits in 4 bytes (contains alpha channel):
+	 * <ul>
+	 * <li><strong>8</strong> bits red</li>
+	 * <li><strong>8</strong> bits green</li>
+	 * <li><strong>8</strong> bits blue</li>
+	 * <li><strong>8</strong> bits alpha</li>
+	 * </ul>
+	 * @see #BGR32
+	 */
 	RGB32(32, IMF_RGB32),
 
-	// Grey formats
-	GREY(8, IMF_GREY),
+	// Gray formats
+	/**
+	 * Grayscale format, <strong>8</strong> bits/pixel.
+	 * Also known as Y8.
+	 */
+	GRAY(8, IMF_GRAY),
+	/**
+	 * Grayscale format; <strong>4</strong> bits/pixel.
+	 */
 	Y4(4, IMF_Y4),
+	/**
+	 * Grayscale format; <strong>6</strong> bits/pixel.
+	 */
 	Y6(6, IMF_Y6),
+	/**
+	 * Grayscale format; <strong>10</strong> bits/pixel.
+	 */
 	Y10(10, IMF_Y10),
+	/**
+	 * Grayscale format; <strong>16</strong> bits/pixel.
+	 */
 	Y16(16, IMF_Y16),
 
 	// palette formats
@@ -110,22 +210,40 @@ public enum ImagePalette {
 	YUV411(16, 77);
 	
 	public static ImagePalette lookup(int palette) {
-		if (palette < 0 || palette > ImagePalette.values().length)
+		if (palette < 0 || palette > ImagePalette.values().length) {
+			System.err.println("Unknown palette ID " + palette + ". Please report this problem to .");
 			return null;
+		}
 		return ImagePalette.values()[palette];
 	}
 	
-	protected final int depth, index;
+	/**
+	 * The color depth for this palette
+	 */
+	private final int depth;
+	
+	/**
+	 * The libVideo index of this palette. Pretty much the same as this's {@link #ordinal()}.
+	 */
+	private final int index;
 	
 	ImagePalette(int depth, int index) {
 		this.depth = depth;
 		this.index = index;
 	}
 	
+	/**
+	 * Get the color depth for this palette. Basically the width of 1 pixel.
+	 * Will be invalid for color formats that are compressed.
+	 * @return this palette's color depth, or -1 if not applicable
+	 */
 	public int getColorDepth() {
 		return this.depth;
 	}
 	
+	/**
+	 * @return the libVideo index of this palette.
+	 */
 	public int getIndex() {
 		return index;
 	}
