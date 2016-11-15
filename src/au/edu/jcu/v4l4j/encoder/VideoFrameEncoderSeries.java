c650bbcc1b98a93009ece6d4afd3b0623b93425f
mailmindlin <liamttlyrox@gmail.com>
2016-11-15 01:15:10 -0500

    Simplified JNI calls

--- src/au/edu/jcu/v4l4j/encoder/VideoFrameEncoderSeries.java
+++ src/au/edu/jcu/v4l4j/encoder/VideoFrameEncoderSeries.java
@@ -1,26 +1,19 @@
 package au.edu.jcu.v4l4j.encoder;
 
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 import au.edu.jcu.v4l4j.ImagePalette;
 import au.edu.jcu.v4l4j.exceptions.BufferOverflowException;
 import au.edu.jcu.v4l4j.exceptions.BufferUnderflowException;
 import au.edu.jcu.v4l4j.exceptions.JNIException;
-import au.edu.jcu.v4l4j.exceptions.V4L4JException;
 
 /**
  * A wrapper for the <code>libv4lconvert</code> library.
  * 
  * @author mailmindlin
  */
-public class VideoFrameEncoderSeries implements VideoFrameEncoder {
-	protected VideoFrameEncoder[] encoders;
-	/**
-	 * A pointer to the native <code>struct v4lconvert_encoder_series</code> in
-	 * memory.
-	 */
-	protected long object;
+public class VideoFrameEncoderSeries implements VideoFrameTransformer {
+	
 	static {
 		try {
 			System.loadLibrary("v4l4j");
@@ -30,6 +23,14 @@ public class VideoFrameEncoderSeries implements VideoFrameEncoder {
 		}
 	}
 	
+	protected VideoFrameTransformer[] encoders;
+	/**
+	 * A pointer to the native <code>struct v4lconvert_encoder_series</code> in
+	 * memory.
+	 */
+	protected long object;
+	
+	private static native long initWithEncoders(long[] encoders);
 	/**
 	 * Create the native struct
 	 * 
@@ -45,81 +46,60 @@ public class VideoFrameEncoderSeries implements VideoFrameEncoder {
 	 * @throws JNIException
 	 *             if there was a problem with allocation or something
 	 */
-	private native long doInit(int from, int to, int width, int height) throws JNIException;
-	
-	protected native int getBufferLimit(int buffer);
+	private static native long doInit(int from, int to, int width, int height) throws JNIException;
 	
 	/**
-	 * Put data into the encoder's buffer
-	 * 
-	 * @param array
-	 *            data to put
-	 * @param length
-	 *            length of data
-	 * @throws V4L4JException
+	 * Apply series to buffer
+	 * @param seriesPtr Pointer to native <code>v4lconvert_encoder_series</code> object
+	 * @param bufPtr Pointer to native <code>v4lconvert_buffer</code> object
+	 * @return Number of bytes written to output buffer
+	 * @throws NullPointerException if the seriesPtr or bufPtr point to an invalid target
 	 */
-	protected native void putBuffer(byte[] array, int length) throws V4L4JException;
+	private static native int doApply(long seriesPtr, long bufPtr);
 	
 	/**
-	 * Get the data from the encoder's buffer
-	 * 
-	 * @param array
-	 * @return length of read data
-	 * @throws V4L4JException
+	 * Get the pointers to the encoders for
+	 * @param seriesObject
+	 * @return
+	 * @throws JNIException
 	 */
-	protected native int getBuffer(byte[] array) throws V4L4JException;
+	private static native long[] getEncoderPointers(long seriesObject) throws JNIException;
 	
-	/**
-	 * Set JPEG image quality
-	 * 
-	 * @param quality
-	 */
-	protected native void setQuality(int quality);
-	
-	protected native void doConvert();
-	
-	/**
-	 * Get the ids of the converters used internally by libv4lconvert.
-	 * <p>
-	 * I don't see how anyone would need to use this method for anything,
-	 * because the actual IDs of the converters may be subject to change, but
-	 * it's here for debugging.
-	 * </p>
-	 * 
-	 * @return IDs of converters used
-	 */
-	public native int[] getConverterIds();
-	
-	public VideoFrameEncoderSeries(VideoFrameEncoder...encoders) {
+	public VideoFrameEncoderSeries(VideoFrameTransformer...encoders) {
 		if (encoders == null || encoders.length < 1)
 			throw new NullPointerException();
 		//validate encoders
-		{
-			VideoFrameEncoder encoder0 = encoders[0];
-			int lastWidth = encoder0.getSourceWidth();
-			int lastHeight = encoder0.getSourceHeight();;
-			ImagePalette lastFmt = encoder0.getSourceFormat();
-			for (int i = 0; i < encoders.length; i++) {
-				VideoFrameEncoder encoder = this.encoders[i];
-				if (encoder == null)
-					throw new NullPointerException("Cannot apply null encoder (#" + i + ")");
-				if (lastWidth != encoder.getSourceWidth() || lastHeight != encoder.getSourceHeight() || lastFmt != encoder.getSourceFormat())
-					throw new IllegalArgumentException(String.format("Illegal conversion between encoders #%d to #%d (%S & %dx%d => %S @ %dx%d)", i, i + 1, lastFmt, lastWidth, lastHeight, encoder.getSourceFormat(), encoder.getSourceWidth(), encoder.getSourceHeight()));
-				lastWidth = encoder.getDestinationWidth();
-				lastHeight = encoder.getDestinationHeight();
-				lastFmt = encoder.getDestinationFormat();
-			}
+		VideoFrameTransformer encoder0 = encoders[0];
+		int lastWidth = encoder0.getSourceWidth();
+		int lastHeight = encoder0.getSourceHeight();;
+		ImagePalette lastFmt = encoder0.getSourceFormat();
+		long[] ptrs = new long[encoders.length];
+		for (int i = 0; i < encoders.length; i++) {
+			VideoFrameTransformer encoder = this.encoders[i];
+			if (encoder == null)
+				throw new NullPointerException("Cannot apply null encoder (#" + i + ")");
+			if (lastWidth != encoder.getSourceWidth() || lastHeight != encoder.getSourceHeight() || lastFmt != encoder.getSourceFormat())
+				throw new IllegalArgumentException(String.format("Illegal conversion between encoders #%d to #%d (%S & %dx%d => %S @ %dx%d)", i, i + 1, lastFmt, lastWidth, lastHeight, encoder.getSourceFormat(), encoder.getSourceWidth(), encoder.getSourceHeight()));
+			if ((ptrs[i] = encoder.getPointer()) == 0)
+				throw new IllegalArgumentException("No pointer available for encoder #" + i + ": " + encoder);
+			lastWidth = encoder.getDestinationWidth();
+			lastHeight = encoder.getDestinationHeight();
+			lastFmt = encoder.getDestinationFormat();
 		}
+		this.object = VideoFrameEncoderSeries.initWithEncoders(ptrs);
 		//Copy the array, so changes to the argument array don't affect the state of the series
-		this.encoders = new VideoFrameEncoder[encoders.length];
+		this.encoders = new VideoFrameTransformer[encoders.length];
 		System.arraycopy(encoders, 0, this.encoders, 0, encoders.length);
 	}
 	
 	protected VideoFrameEncoderSeries(int width, int height, ImagePalette from, ImagePalette to) {
 		this.object = doInit(from.getIndex(), to.getIndex(), width, height);
 		
+		long[] converters = VideoFrameEncoderSeries.getEncoderPointers(this.object);
+		this.encoders = new VideoFrameTransformer[converters.length];
+		for (int i = 0; i < converters.length; i++)
+			this.encoders[i] = VideoFrameConverter.wrap(converters[i]);
 		System.out.println("Built encoder from " + from + " to " + to + '.');
-		System.out.println("\tIds " + Arrays.toString(getConverterIds()));
 	}
 	
 	@Override
@@ -130,11 +110,20 @@ public class VideoFrameEncoderSeries implements VideoFrameEncoder {
 		return -1;
 	}
 	
+	/**
+	 * Get length of chain of converters
+	 * @return length
+	 */
 	public int getLength() {
 		return this.encoders.length;
 	}
 	
-	public VideoFrameEncoder getEncoder(int index) {
+	/**
+	 * Get converter by index
+	 * @param index
+	 * @return converter
+	 */
+	public VideoFrameTransformer getEncoder(int index) {
 		return this.encoders[index];
 	}
 	
@@ -181,8 +170,7 @@ public class VideoFrameEncoderSeries implements VideoFrameEncoder {
 	
 	@Override
 	public int apply(V4lconvertBuffer buf) {
-		// TODO Auto-generated method stub
-		return -1;
+		return VideoFrameEncoderSeries.doApply(this.object, buf.getPointer());
 	}
 	
 	public V4lconvertBuffer createBuffer() {
@@ -191,8 +179,14 @@ public class VideoFrameEncoderSeries implements VideoFrameEncoder {
 	}
 	
 	@Override
-	public int apply(ByteBuffer src, ByteBuffer dst)
-			throws BufferUnderflowException, BufferOverflowException, IllegalArgumentException {
+	public int apply(ByteBuffer src, ByteBuffer dst) throws BufferUnderflowException, BufferOverflowException, IllegalArgumentException {
+		//Only supporting V4lconvertBuffer's
 		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public long getPointer() {
+		//No available v4lconvert_converter pointer (maybe will change in the future)
+		return 0;
 	}
 }
