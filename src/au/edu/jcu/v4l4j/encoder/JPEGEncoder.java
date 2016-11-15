53daf55ce676e83d978764a87cdffdeb1cefd40f
mailmindlin <liamttlyrox@gmail.com>
2016-11-15 01:11:08 -0500

    Simplified JPEG encoder
    Passes all tests!

--- src/au/edu/jcu/v4l4j/encoder/JPEGEncoder.java
+++ src/au/edu/jcu/v4l4j/encoder/JPEGEncoder.java
@@ -1,36 +1,31 @@
 package au.edu.jcu.v4l4j.encoder;
 
-import au.edu.jcu.v4l4j.BaseVideoFrame;
 import au.edu.jcu.v4l4j.ImagePalette;
 import au.edu.jcu.v4l4j.V4L4JConstants;
-import au.edu.jcu.v4l4j.VideoFrame;
-import au.edu.jcu.v4l4j.exceptions.V4L4JException;
+import au.edu.jcu.v4l4j.exceptions.JNIException;
 
 public class JPEGEncoder extends ImageFormatConverter {
-	
-	protected int quality;
 	
 	public static JPEGEncoder from(int width, int height, ImagePalette from) {
 		return new JPEGEncoder(width, height, from);
 	}
 	
-	public static JPEGEncoder to(int width, int height, ImagePalette to) {
-		return new JPEGEncoder(width, height, ImagePalette.JPEG, to);
+	protected JPEGEncoder(long ptr) {
+		super(ptr);
 	}
 	
-	protected JPEGEncoder(int width, int height, ImagePalette from, ImagePalette to) {
-		super(ImageFormatConverter.getConverterIDForTransformation(from, to), width, height);
+	protected JPEGEncoder(int width, int height, ImagePalette from) {
+		super(ImageFormatConverter.lookupConverterByConversion(from, ImagePalette.JPEG), width, height);
 	}
 	
-	public JPEGEncoder(int width, int height, ImagePalette from) {
-		this(width, height, from, ImagePalette.JPEG);
-	}
-
-	public void setQuality(int quality) {
-		if (quality < V4L4JConstants.MIN_JPEG_QUALITY)
-			quality = V4L4JConstants.MIN_JPEG_QUALITY;
-		if (quality > V4L4JConstants.MAX_JPEG_QUALITY)
-			quality = V4L4JConstants.MAX_JPEG_QUALITY;
-		this.quality = quality;
-	}
+	/**
+	 * Set encoded JPEG quality
+	 * @param quality
+	 */
+	public native void setQuality(int quality);
+	/**
+	 * Get encoded JPEG quality
+	 * @return
+	 */
+	public native int getQuality();
 }
