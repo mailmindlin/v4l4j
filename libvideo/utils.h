3e127497a9668162b23bcf6062b8af79e76b52b7
mailmindlin <liamttlyrox@gmail.com>
2016-11-16 00:30:55 -0500

    Moved macro to more public place
    Pretty useful.

--- libvideo/utils.h
+++ libvideo/utils.h
@@ -4,4 +4,9 @@
 #define CLEAR_ARR(x,s) memset((x), 0x0, (s));
 #define ARRAY_SIZE(x) ( ((x)==NULL) ? 0 : (sizeof(x)/sizeof((x)[0])) )
 
+#ifndef UNUSED
+// To hide unused parameter warnings
+#define UNUSED(x) (void)(x)
+#endif
+
 #endif
