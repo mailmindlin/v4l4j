2b2e8103dfdd325f1109624e870fe87d5ba95b60
mailmindlin <liamttlyrox@gmail.com>
2016-11-16 00:23:11 -0500

    Added more options for conversion algorithms
    Now offering fixed-point math

--- libvideo/libv4lconvert/rgbyuv.c
+++ libvideo/libv4lconvert/rgbyuv.c
@@ -94,54 +94,80 @@ void v4lconvert_rgb24_to_yuv420(const u8 *src, u8 *dest, const struct v4l2_forma
 	}
 }
 
-#ifdef GOOD_CONVERSION
-	// Relatively slow conversion, but nice and accurate
-	// If the processor has a fpu, these shouldn't be much worse
-	#define UV2V1(u, v) (int) (0.00000f * (u) + 1.13983f * (v) + 0.5f)
-	#define UV2RG(u, v) (int) (0.39465f * (u) + 0.58060f * (v) + 0.5f)
-	#define UV2U1(u, v) (int) (2.03211f * (u) + 0.00000f * (v) + 0.5f)
-#elif defined(FAST_CONVERSION)
+#define SCALEBITS 		10
+#define ONE_HALF		(1UL << (SCALEBITS - 1))	
+#define CLIP256(color)	(u8)({int _color = (color);(_color > 0xFF) ? 0xFF : ((_color < 0) ? 0 : _color);})
+#define CLIP(color)		CLIP256(color)
+#if defined(CONVERSION_QUALITY) && (CONVERSION_QUALITY < 0 || CONVERSION_QUALITY > 3)
+	#undef CONVERSION_QUALITY
+#endif
+#ifndef CONVERSION_QUALITY
+	#pragma message "Defaulting CONVERSION_QUALITY to 3 (full float)"
+	#define CONVERSION_QUALITY 3
+#endif
+#if CONVERSION_QUALITY == 0
 	// Optimized for speed, at the expense of precision.
 	// Might not be worth it nowdays if you have a co-processor or FPU
-	#define UV2V1(u, v) ((((v) << 1) + (v)) >> 1)
-	#define UV2RG(u, v) (((u << 1) + (u) + ((v) << 2) + ((v) << 1)) >> 3)
-	#define UV2U1(u, v) ((((u) << 7) + (u)) >> 6)
-#else
+	#define FIX(x)			(x)
+	#define UV2V1(u, v)		((((v) << 1) + (v)) >> 1)
+	#define UV2RG(u, v)		(((u << 1) + (u) + ((v) << 2) + ((v) << 1)) >> 3)
+	#define UV2U1(u, v)		((((u) << 7) + (u)) >> 6)
+#elif CONVERSION_QUALITY == 1
 	// Integer approximation. Middling speed
-	#define UV2V1(u, v) (((v) * 1436) >> 10)
-	#define UV2RG(u, v) (((u) * 352 + ((v) * 731)) >> 10)
-	#define UV2U1(u, v) (((u) * 1814) >> 10)
+	#define FIX(x)			(x)
+	#define UV2V1(u, v)		(((v) * 1436) >> 10)
+	#define UV2RG(u, v)		(((u) * 352 + ((v) * 731)) >> 10)
+	#define UV2U1(u, v)		(((u) * 1814) >> 10)
+#elif CONVERSION_QUALITY == 2
+	// Fixed-point arithmetic. Kinda fast, and should get you the precision you need
+	// Can still be beat out by some FPU's, just because it requires more instructions
+	#define FIX(x)			((int)((x) * (1UL << SCALEBITS) + 0.5))
+	#define UV2V1(u, v)		(FIX(1.40200) * v + ONE_HALF)
+	#define UV2RG(u, v)		(FIX(0.34414) * u + FIX(0.71414) * v - ONE_HALF)
+	#define UV2U1(u, v)		(FIX(1.77200) * u + ONE_HALF)
+	#define CLIP(color)		CLIP256((color) >> SCALEBITS)
+#elif CONVERSION_QUALITY == 3
+	// Relatively slow conversion, but nice and accurate
+	// If the processor has a fpu, these shouldn't be much worse
+	#define FIX(x)			(x)
+	#define UV2V1(u, v)		(int) (0.00000f * (float) (u) + 1.13983f * (float) (v) + 0.5f)
+	#define UV2RG(u, v)		(int) (0.39465f * (float) (u) + 0.58060f * (float) (v) - 0.5f)
+	#define UV2U1(u, v)		(int) (2.03211f * (float) (u) + 0.00000f * (float) (v) + 0.5f)
 #endif
-#define CLIP(color) (u8)(((color) > 0xFF) ? 0xFF : (((color) < 0) ? 0 : (color)))
 
 void v4lconvert_yuv420_to_bgr24(const u8 *src, u8 *dest, u32 width, u32 height, int yvu) {
 	const u8 *ysrc = src;
-	const u8 *usrc, *vsrc;
-
+	const u8 *usrc = src + width * height;
+	const u8 *vsrc = usrc + (width * height) / 4;
+	
 	if (yvu) {
-		vsrc = src + width * height;
-		usrc = vsrc + (width * height) / 4;
-	} else {
-		usrc = src + width * height;
-		vsrc = usrc + (width * height) / 4;
+		const u8* tmp = usrc;
+		usrc = vsrc;
+		vsrc = tmp;
 	}
+	
+	v4lconvert_cvt_yuv420_to_bgr24(ysrc, usrc, vsrc, dest, width, height);
+}
 
+__attribute__((inline)) void v4lconvert_cvt_yuv420_to_bgr24(const u8 *ysrc, const u8* usrc, const u8* vsrc, u8 *dest, u32 width, u32 height) {
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
+			int y = FIX(*ysrc++);
 			int u = *usrc++ - 128;
 			int v = *vsrc++ - 128;
+			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
 			int v1 = UV2V1(u, v);
-			*dest++ = CLIP(*ysrc + u1);
-			*dest++ = CLIP(*ysrc - rg);
-			*dest++ = CLIP(*ysrc + v1);
-			ysrc++;
+			
+			*dest++ = CLIP(y + u1);
+			*dest++ = CLIP(y - rg);
+			*dest++ = CLIP(y + v1);
 
-			*dest++ = CLIP(*ysrc + u1);
-			*dest++ = CLIP(*ysrc - rg);
-			*dest++ = CLIP(*ysrc + v1);
-			ysrc++;
+			y = FIX(*ysrc++);
+			*dest++ = CLIP(y + u1);
+			*dest++ = CLIP(y - rg);
+			*dest++ = CLIP(y + v1);
 		}
 		/* Rewind u and v for next line */
 		if (!(i & 1)) {
@@ -165,21 +191,21 @@ void v4lconvert_yuv420_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height, 
 
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
+			int y = FIX(*ysrc++);
 			int u = *usrc++ - 128;
 			int v = *vsrc++ - 128;
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
 			int v1 = UV2V1(u, v);
 
-			*dest++ = CLIP(*ysrc + v1);
-			*dest++ = CLIP(*ysrc - rg);
-			*dest++ = CLIP(*ysrc + u1);
-			ysrc++;
-
-			*dest++ = CLIP(*ysrc + v1);
-			*dest++ = CLIP(*ysrc - rg);
-			*dest++ = CLIP(*ysrc + u1);
-			ysrc++;
+			*dest++ = CLIP(y + v1);
+			*dest++ = CLIP(y - rg);
+			*dest++ = CLIP(y + u1);
+			
+			y = FIX(*ysrc++);
+			*dest++ = CLIP(y + v1);
+			*dest++ = CLIP(y - rg);
+			*dest++ = CLIP(y + u1);
 		}
 		/* Rewind u and v for next line */
 		if (!(i&1)) {
@@ -192,10 +218,10 @@ void v4lconvert_yuv420_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height, 
 void v4lconvert_yuyv_to_bgr24(const u8 *src, u8 *dest, u32 width, u32 height) {
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
-			int y1 = *(src++);
-			int u = *(src++) - 128;
-			int y2 = *(src++);
-			int v = *(src++) - 128;
+			int y1 = FIX(*src++);
+			int u = *src++ - 128;
+			int y2 = FIX(*src++);
+			int v = *src++ - 128;
 			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
@@ -219,10 +245,10 @@ void v4lconvert_yuyv_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height) {
 #else
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
-			int y1 = *(src++);
-			int u = *(src++) - 128;
-			int y2 = *(src++);
-			int v = *(src++) - 128;
+			int y1 = FIX(*src++);
+			int u = *src++ - 128;
+			int y2 = FIX(*src++);
+			int v = *src++ - 128;
 			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
@@ -285,10 +311,10 @@ void v4lconvert_yuyv_to_yuv420(const u8 *src, u8 *dest, u32 width, u32 height, i
 void v4lconvert_yvyu_to_bgr24(const u8 *src, u8 *dest, u32 width, u32 height) {
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
-			int y1 = *(src++);
-			int v = *(src++) - 128;
-			int y2 = *(src++);
-			int u = *(src++) - 128;
+			int y1 = FIX(*src++);
+			int v = *src++ - 128;
+			int y2 = FIX(*src++);
+			int u = *src++ - 128;
 			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
@@ -308,10 +334,10 @@ void v4lconvert_yvyu_to_bgr24(const u8 *src, u8 *dest, u32 width, u32 height) {
 void v4lconvert_yvyu_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height) {
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
-			int y1 = *(src++);
-			int v = *(src++) - 128;
-			int y2 = *(src++);
-			int u = *(src++) - 128;
+			int y1 = FIX(*src++);
+			int v = *src++ - 128;
+			int y2 = FIX(*src++);
+			int u = *src++ - 128;
 			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
@@ -331,10 +357,10 @@ void v4lconvert_yvyu_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height) {
 void v4lconvert_uyvy_to_bgr24(const u8 *src, u8 *dest, u32 width, u32 height) {
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
-			int u = *(src++) - 128;
-			int y1 = *(src++);
-			int v = *(src++) - 128;
-			int y2 = *(src++);
+			int u = *src++ - 128;
+			int y1 = FIX(*src++);
+			int v = *src++ - 128;
+			int y2 = FIX(*src++);
 			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
@@ -354,10 +380,10 @@ void v4lconvert_uyvy_to_bgr24(const u8 *src, u8 *dest, u32 width, u32 height) {
 void v4lconvert_uyvy_to_rgb24(const u8 *src, u8 *dest, u32 width, u32 height) {
 	for (unsigned int i = 0; i < height; i++) {
 		for (unsigned int j = 0; j < width; j += 2) {
-			int u = *(src++) - 128;
-			int y1 = *(src++);
-			int v = *(src++) - 128;
-			int y2 = *(src++);
+			int u = *src++ - 128;
+			int y1 = FIX(*src++);
+			int v = *src++ - 128;
+			int y2 = FIX(*src++);
 			
 			int u1 = UV2U1(u, v);
 			int rg = UV2RG(u, v);
