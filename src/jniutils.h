d094a18015bbec02e8dc21613541f0db9c2c3cd9
mailmindlin <liamttlyrox@gmail.com>
2016-11-17 00:52:38 -0500

    Fixed attributes

--- src/jniutils.h
+++ src/jniutils.h
@@ -6,12 +6,11 @@
 #ifndef __JNIUTILS_H
 #define __JNIUTILS_H
 
-jmethodID lookupAddMethod(JNIEnv *env, jobject list) __attribute__ ((pure), (visibility ("hidden")));
-jclass lookupClassSafe(JNIEnv *env, jobject obj) __attribute__ ((pure), (visibility ("hidden")));
-int getBufferPosition(JNIEnv *env, jobject buffer) __attribute__ ((pure), (visibility ("hidden")));
-unsigned char* getBufferPointer(JNIEnv *env, jobject buffer, jbyteArray* arrayRef, unsigned int* len) __attribute__((visibility ("hidden")));
+jmethodID lookupAddMethod(JNIEnv *env, jobject list) __attribute__ ((pure, visibility ("hidden")));
+jclass lookupClassSafe(JNIEnv *env, jobject obj) __attribute__ ((pure, visibility ("hidden")));
+int getBufferPosition(JNIEnv *env, jobject buffer) __attribute__ ((pure, visibility ("hidden")));
 void setBufferPosition(JNIEnv *env, jobject buffer, int position) __attribute__((visibility ("hidden")));
 void setBufferLimit(JNIEnv *env, jobject buffer, int limit) __attribute__((visibility ("hidden")));
-int getBufferLimit(JNIEnv *env, jobject limit) __attribute__ ((pure), (visibility ("hidden")));
-unsigned char* getBufferPointer(JNIEnv *env, jobject buffer, jbyteArray* arrayRef, unsigned int* len, void (*release)(JNIEnv* env, jbyteArray* arrayRef, unsigned char* ptr)) __attribute__((visibility ("hidden")));
+int getBufferLimit(JNIEnv *env, jobject limit) __attribute__ ((pure, visibility ("hidden")));
+unsigned char* getBufferPointer(JNIEnv *env, jobject buffer, jbyteArray* arrayRef, unsigned int* len, void (**release)(JNIEnv* env, jbyteArray arrayRef, unsigned char* ptr)) __attribute__((visibility ("hidden")));
 #endif
