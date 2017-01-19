3566e662692402f6354b96827dfdc85be9d3325f
mailmindlin <liamttlyrox@gmail.com>
2017-01-18 22:36:56 -0500

    Fix method visibility

--- libvideo/libv4lconvert/libv4lconvert-wrappers.h
+++ libvideo/libv4lconvert/libv4lconvert-wrappers.h
@@ -1,7 +1,9 @@
 #ifndef __LIBV4LCONVERT_WRAPPERS_H
 #define __LIBV4LCONVERT_WRAPPERS_H
 
-ImageTransformer* ImageTransformer_imf_init(ImageTransformerPrototype* self, struct v4l2_format* src_fmt, struct v4l2_format* dst_fmt, char** errmsg, size_t options_len, ...);
-ImageTransformer* ImageTransformer_jpeg_init(ImageTransformerPrototype* self, struct v4l2_format* src_fmt, struct v4l2_format* dst_fmt, char** errmsg, size_t options_len, ...);
+#include "libv4lconvert-priv.h"
 
-#endif
+LIBV4LCONVERT_LOCAL ImageTransformer* ImageTransformer_imf_init(ImageTransformerPrototype* self, struct v4l2_format* src_fmt, struct v4l2_format* dst_fmt, char** errmsg, size_t options_len, ...);
+LIBV4LCONVERT_LOCAL ImageTransformer* ImageTransformer_jpeg_init(ImageTransformerPrototype* self, struct v4l2_format* src_fmt, struct v4l2_format* dst_fmt, char** errmsg, size_t options_len, ...);
+
+#endif //__LIBV4LCONVERT_WRAPPERS_H
