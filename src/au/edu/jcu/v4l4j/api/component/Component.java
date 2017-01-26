62f4a6bb54da57cd7a505eb75268ecf96d6ad410
mailmindlin <liamttlyrox@gmail.com>
2017-01-26 12:35:58 -0500

    Add Component#getRoles()

--- src/au/edu/jcu/v4l4j/api/component/Component.java
+++ src/au/edu/jcu/v4l4j/api/component/Component.java
@@ -7,23 +7,43 @@ import au.edu.jcu.v4l4j.api.component.port.ImagePort;
 import au.edu.jcu.v4l4j.api.component.port.VideoPort;
 
 public interface Component {
+	/**
+	 * Get component name
+	 * @return
+	 */
 	String getName();
+	
 	ComponentProvider getProvider();
+	
 	/**
 	 * Get the component's current state
 	 * @return the component's current state
 	 */
 	ComponentState getState();
+	
 	/**
 	 * Attempt to set the component's state
 	 * @param state
 	 * @return the state of the component after this call
 	 */
 	ComponentState setState(ComponentState state);
+	
+	/**
+	 * Enumerate all the ports on this component
+	 * @return
+	 */
 	Set<ComponentPort> getPorts();
 	Set<AudioPort> getAudioPorts();
 	Set<ImagePort> getImagePorts();
 	Set<VideoPort> getVideoPorts();
 	Set<ComponentPort> getOtherPorts();
+	
+	/**
+	 * Get the port at the given index
+	 * @param index
+	 * @return
+	 */
 	ComponentPort getPort(int index);
+	
+	Set<ComponentRole> getRoles();
 }
