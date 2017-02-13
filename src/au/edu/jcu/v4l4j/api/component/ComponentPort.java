e84a7b23fa6c82d52de5705ff5e9caf5341287ea
mailmindlin <liamttlyrox@gmail.com>
2017-02-13 15:12:32 -0500

    Ports extend ComposteControl
    Maybe will change in future, but it works for now...

--- src/au/edu/jcu/v4l4j/api/component/ComponentPort.java
+++ src/au/edu/jcu/v4l4j/api/component/ComponentPort.java
@@ -1,23 +1,17 @@
 package au.edu.jcu.v4l4j.api.component;
 
 import java.nio.ByteBuffer;
-import java.util.Set;
 
 import au.edu.jcu.v4l4j.api.FrameBuffer;
 import au.edu.jcu.v4l4j.api.StreamType;
+import au.edu.jcu.v4l4j.api.control.CompositeControl;
 
-public interface ComponentPort {
+public interface ComponentPort extends CompositeControl {
 	int getIndex();
 	
 	StreamType getPortType();
 	
 	Component getComponent();
-	
-	Set<String> getPropertyNames();
-	
-	Object getProperty(String key);
-	
-	Object setProperty(String key, Object value);
 	
 	boolean isInput();
 	
