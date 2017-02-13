565bc5b5a0950baf3cb0b178e282432ae7883887
mailmindlin <liamttlyrox@gmail.com>
2017-02-13 13:53:04 -0500

    More OMX api stuff

--- src/au/edu/jcu/v4l4j/api/control/Control.java
+++ src/au/edu/jcu/v4l4j/api/control/Control.java
@@ -3,4 +3,14 @@ package au.edu.jcu.v4l4j.api.control;
 public interface Control {
 	ControlType getType();
 	String getName();
+	/**
+	 * Pushes the control's state in memory to the device.
+	 * Operates on sub-controls
+	 */
+	void push();
+	/**
+	 * Updates the control's memory state from the device
+	 * Operates on sub-controls.
+	 */
+	void pull();
 }
---
+++ src/au/edu/jcu/v4l4j/impl/omx/OMXComponentPort.java
@@ -0,0 +1,6 @@
+package au.edu.jcu.v4l4j.impl.omx;
+
+public abstract class OMXComponentPort implements ComponentPort {
+	protected final int id;
+	protected final OMXComponent component;
+}
