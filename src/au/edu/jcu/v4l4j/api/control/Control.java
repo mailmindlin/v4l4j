dc581d885f2ac7ed1f65c217a4c7cc5ee15e48fb
mailmindlin <liamttlyrox@gmail.com>
2017-02-21 00:23:02 -0500

    Javadoc Control

--- src/au/edu/jcu/v4l4j/api/control/Control.java
+++ src/au/edu/jcu/v4l4j/api/control/Control.java
@@ -1,13 +1,26 @@
 package au.edu.jcu.v4l4j.api.control;
 
 public interface Control extends AutoCloseable {
+	/**
+	 * Get the type of this control.
+	 * The returned value for this method should be constant across calls.
+	 * @return Type of this control
+	 */
 	ControlType getType();
+	
+	/**
+	 * Get the name of this control.
+	 * The returned value for this method should be constant across calls.
+	 * @return this control's name
+	 */
 	String getName();
+	
 	/**
 	 * Pushes the control's state in memory to the device.
 	 * Operates on sub-controls
 	 */
 	void push();
+	
 	/**
 	 * Updates the control's memory state from the device
 	 * Operates on sub-controls.
--- src/au/edu/jcu/v4l4j/api/control/ControlType.java
+++ src/au/edu/jcu/v4l4j/api/control/ControlType.java
@@ -1,7 +1,13 @@
 package au.edu.jcu.v4l4j.api.control;
 
 public enum ControlType {
+	/**
+	 * A button control.
+	 */
 	BUTTON,
+	/**
+	 * A range slider
+	 */
 	SLIDER,
 	SWITCH,
 	DISCRETE,
