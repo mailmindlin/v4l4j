f32ae0accfc5e4c75615a0e0d78df7491621c8e3
mailmindlin <liamttlyrox@gmail.com>
2017-02-21 12:37:12 -0500

    Merge ValuedControl into Control (not git merge)
    Some fine-tuning needed

--- src/au/edu/jcu/v4l4j/api/control/Control.java
+++ src/au/edu/jcu/v4l4j/api/control/Control.java
@@ -1,6 +1,12 @@
 package au.edu.jcu.v4l4j.api.control;
 
-public interface Control extends AutoCloseable {
+import java.time.Duration;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public interface Control<T> extends AutoCloseable {
 	/**
 	 * Get the type of this control.
 	 * The returned value for this method should be constant across calls.
@@ -15,17 +21,46 @@ public interface Control extends AutoCloseable {
 	 */
 	String getName();
 	
-	/**
-	 * Pushes the control's state in memory to the device.
-	 * Operates on sub-controls
-	 */
-	void push();
-	
-	/**
-	 * Updates the control's memory state from the device
-	 * Operates on sub-controls.
-	 */
-	void pull();
-	
 	void close();
+	
+	ControlGetter<T, T> get();
+	
+	ControlAccessor<T, Void> access();
+	
+	public static interface ControlAccessor<T, R> extends Callable<R> {
+		ControlAccessor<T, R> setTimeout(Duration timeout);
+		
+		ControlGetter<T, T> get();
+		
+		@Override
+		R call() throws Exception;
+	}
+	
+	public static interface ControlGetter<T, R> extends ControlAccessor<T, R> {
+		
+		@Override
+		ControlGetter<T, R> setTimeout(Duration timeout);
+		
+		ControlGetter<T, R> read(Consumer<T> handler);
+		
+		ControlUpdater<T, R> write(T value);
+		
+		ControlUpdater<T, R> write(Supplier<T> supplier);
+		
+		ControlUpdater<T, R> update(Function<T, T> mappingFunction);
+		
+		ControlUpdater<T, R> increase();
+		
+		ControlUpdater<T, R> decrease();
+	}
+	
+	public static interface ControlUpdater<T, R> extends ControlGetter<T, R> {
+		
+		@Override
+		ControlUpdater<T, R> setTimeout(Duration timeout);
+		
+		ControlAccessor<T, R> set();
+		
+		ControlGetter<T, R> setAndGet();
+	}
 }
