a44b1dae4be3f7d8700b0694dea1619ff5422d1d
mailmindlin <liamttlyrox@gmail.com>
2017-02-22 21:54:45 -0500

    Add testing program for OMX
    Definately will remove eventually

---
+++ src/au/edu/jcu/v4l4j/test/OMXTest.java
@@ -0,0 +1,147 @@
+package au.edu.jcu.v4l4j.test;
+
+import java.nio.file.Paths;
+import java.util.Set;
+
+import au.edu.jcu.v4l4j.api.FrameBuffer;
+import au.edu.jcu.v4l4j.api.ImagePalette;
+import au.edu.jcu.v4l4j.api.component.ComponentPort;
+import au.edu.jcu.v4l4j.api.component.ComponentState;
+import au.edu.jcu.v4l4j.api.component.port.VideoPort;
+import au.edu.jcu.v4l4j.impl.jni.MemoryUtils;
+import au.edu.jcu.v4l4j.impl.jni.PrimitiveStructFieldType;
+import au.edu.jcu.v4l4j.impl.jni.StructMap;
+import au.edu.jcu.v4l4j.impl.jni.StructPrototype;
+import au.edu.jcu.v4l4j.impl.omx.OMXComponent;
+import au.edu.jcu.v4l4j.impl.omx.OMXComponentProvider;
+import au.edu.jcu.v4l4j.impl.omx.OMXConstants;
+import au.edu.jcu.v4l4j.impl.omx.OMXQuery;
+
+public class OMXTest {
+	public static void main(String...fred) throws Exception {
+		for (PrimitiveStructFieldType type : PrimitiveStructFieldType.values())
+			System.out.println(type.name() + ";" + type.getAlignment() + ";" + type.getSize());
+		OMXComponentProvider provider = OMXComponentProvider.getInstance();
+		for (String name : provider.availableNames(Paths.get("/"), null))
+			System.out.println(name);
+		OMXComponent encoder = (OMXComponent) provider.get("OMX.broadcom.video_encode");
+		System.out.println("Encoder name: " + encoder.getName());
+		System.out.println("Encoder state: " + encoder.getState());
+		
+		testAccess(encoder.getPointer());
+		
+		testQueryPorts(encoder);
+		
+		Set<ComponentPort> ports = encoder.getPorts();
+		System.out.println("Ports: " + ports);
+		
+		for (ComponentPort port : ports) {
+			System.out.println("Port " + port.getIndex());
+			System.out.println("\t=>MIME:\t" + port.getMIMEType());
+			System.out.println("\t=>Type:\t" + port.getPortType());
+			System.out.println("\t=>MinB:\t" + port.minimumBuffers());
+			System.out.println("\t=>ActB:\t" + port.actualBuffers());
+			System.out.println("\t=>szBf:\t" + port.bufferSize());
+			System.out.println("\t=>Dir: \t" + (port.isInput() ? "INPUT" : "OUTPUT"));
+			System.out.println("\t=>Enbl:\t" + (port.isEnabled() ? "ENABLED" : "DISABLED"));
+		}
+		
+		testQueryPortdef(encoder);
+		
+		VideoPort inputPort = (VideoPort) encoder.getPort(200);
+		VideoPort outputPort = (VideoPort) encoder.getPort(201);
+		
+		inputPort.setEnabled(false);
+		outputPort.setEnabled(false);
+		
+		System.out.println("Transitioned to state " + encoder.setState(ComponentState.IDLE));
+		
+		inputPort.setEnabled(true);
+		outputPort.setEnabled(true);
+		
+		FrameBuffer inBuffer = inputPort.allocateBuffer(inputPort.bufferSize());
+		FrameBuffer outBuffer = outputPort.allocateBuffer(outputPort.bufferSize());
+		inBuffer.toString();
+		outBuffer.toString();
+	}
+	
+	public static void testAccess(long pointer) {
+		System.out.println("Pointer: 0x" + Long.toHexString(pointer));
+		
+		StructPrototype proto = StructPrototype.builder()
+				.addInt32("nSize")
+				.addStruct(OMXQuery.VERSION_TYPE, "version")
+				.build();
+		
+		StructPrototype adProto = StructPrototype.builder()
+				.addPointer(proto, "component")
+				.addPointer("callbacks")
+				.build();
+		
+		StructMap data = new StructMap(adProto, pointer, 16);
+		System.out.println("Component pointer: " + Long.toHexString(((Number)data.get("component")).longValue()));
+		data.wrapFar("component");
+		StructMap component = data.getStruct("component");
+		System.out.println("component struct size: " + ((Number)component.get("nSize")).intValue());
+		StructMap componentVsn = component.getStruct("version");
+		System.out.println("Version major " + componentVsn.get("nVersionMajor"));
+		System.out.println("Version minor " + componentVsn.get("nVersionMinor"));
+		System.out.println("Version revis " + componentVsn.get("nRevision"));
+		System.out.println("Version step  " + componentVsn.get("nStep"));
+		try {
+			data.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void testQueryPorts(OMXComponent component) throws Exception {
+		StructPrototype portQueryProto = StructPrototype.builder()
+			.addInt32("size")
+			.addStruct(OMXQuery.VERSION_TYPE, "version")
+			.addInt32("length")
+			.addInt32("startPortNumber")
+			.build();
+		try (StructMap query = new StructMap(portQueryProto)) {
+			component.accessConfig(false, true, OMXConstants.INDEX_ParamVideoInit, query.buffer());
+			System.out.println("Struct size: " + query.get("size"));
+			StructMap componentVsn = query.getStruct("version");
+			System.out.println("Version major " + componentVsn.get("nVersionMajor"));
+			System.out.println("Version minor " + componentVsn.get("nVersionMinor"));
+			System.out.println("Version revis " + componentVsn.get("nRevision"));
+			System.out.println("Version step  " + componentVsn.get("nStep"));
+			System.out.println("Size: " + query.get("length"));
+			System.out.println("Start: " + query.get("startPortNumber"));
+		}
+	}
+	
+	public static void testQueryPortdef(OMXComponent component) throws Exception {
+		final int FRAME_WIDTH = 1920/4;
+		final int FRAME_HEIGHT = 1080/4;
+		final int FRAMERATE = 25;
+		final int BITRATE = 10000000;
+		try (StructMap query = new StructMap(OMXConstants.PARAM_PORTDEFINITIONTYPE)) {
+			query.put("nPortIndex", 200);
+			component.accessConfig(false, true, OMXConstants.INDEX_ParamPortDefinition, query.buffer());
+			for (String key : query.keySet())
+				System.out.println("\t=>" + key + ":" + query.get(key));
+			StructMap videoDef = query.getUnion("format").getStruct("video");
+			videoDef.put("nFrameWidth", FRAME_WIDTH);
+			videoDef.put("nFrameHeight", FRAME_HEIGHT);
+			videoDef.put("xFramerate", FRAMERATE << 16);
+			//videoDef.put("nBitrate", BITRATE);
+			videoDef.put("nStride", MemoryUtils.align(16, FRAME_WIDTH));
+			videoDef.put("eColorFormat", 20);
+			for (String key : query.keySet())
+				System.out.println("\t->" + key + ":" + query.get(key));
+			component.accessConfig(false, false, OMXConstants.INDEX_ParamPortDefinition, query.buffer());
+			System.out.println("Setting port 201");
+			component.accessConfig(false, true, OMXConstants.INDEX_ParamPortDefinition, query.buffer());
+			query.put("nPortIndex", 201);
+			videoDef.put("eColorFormat", 0);
+			videoDef.put("eCompressionFormat", OMXConstants.unmapVideoEncodingType(ImagePalette.AVC));
+			videoDef.put("nBitrate", BITRATE);
+			component.accessConfig(false, false, OMXConstants.INDEX_ParamPortDefinition, query.buffer());
+		}
+	}
+}
