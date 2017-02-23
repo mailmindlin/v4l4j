07f60b591dc60a398da66a9bc3389b3df7c12228
mailmindlin <liamttlyrox@gmail.com>
2017-02-22 21:50:35 -0500

    Mostly finished implementing NativeUnion

--- src/au/edu/jcu/v4l4j/impl/jni/NativeUnion.java
+++ src/au/edu/jcu/v4l4j/impl/jni/NativeUnion.java
@@ -1,20 +1,35 @@
 package au.edu.jcu.v4l4j.impl.jni;
 
 import java.nio.ByteBuffer;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
-public class NativeUnion implements AutoCloseable, NativeWrapper {
+public class NativeUnion implements AutoCloseable, NativeWrapper, Map<String, Object> {
+	protected final UnionPrototype union;
+	protected final long pointer;
+	protected final ByteBuffer buffer;
+	protected final Map<String, NativeWrapper> wrappers = new HashMap<>();
+	
 	public NativeUnion(UnionPrototype union, ByteBuffer buffer) {
-		
+		this(union, MemoryUtils.unwrap(buffer), buffer, false);
+	}
+	
+	protected NativeUnion(UnionPrototype union, long pointer, ByteBuffer buffer, boolean releaseOnClose) {
+		this.union = union;
+		this.pointer = pointer;
+		this.buffer = buffer;
 	}
 	
 	@Override
 	public long pointer() {
-		return 0;
+		return this.pointer;
 	}
 	
 	@Override
 	public UnionPrototype type() {
-		return null;
+		return this.union;
 	}
 
 	@Override
@@ -25,6 +40,96 @@ public class NativeUnion implements AutoCloseable, NativeWrapper {
 
 	@Override
 	public ByteBuffer buffer() {
+		return this.buffer;
+	}
+
+	@Override
+	public int size() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(Object key) {
+		return this.type().readField(this.buffer(), key.toString());
+	}
+	
+	public StructMap getStruct(String name) {
+		return (StructMap) this.wrappers.computeIfAbsent(name, key->{
+			StructField field = this.type().getOption(key);
+			return new StructMap((StructPrototype) field.getType(), this.pointer(), this.buffer(), false);
+		});
+	}
+	
+	public NativeArray getArray(String name) {
+		return (NativeArray) this.wrappers.computeIfAbsent(name, key->{
+			StructField field = this.type().getOption(key);
+			return new NativeArray((ArrayStructFieldType) field.getType(), this.pointer(), this.buffer(), false);
+		});
+	}
+	
+	public NativeUnion getUnion(String name) {
+		return (NativeUnion) this.wrappers.computeIfAbsent(name, key->{
+			StructField field = this.type().getOption(key);
+			return new NativeUnion((UnionPrototype) field.getType(), this.pointer(), this.buffer(), false);
+		});
+	}
+
+	@Override
+	public Object put(String key, Object value) {
+		this.type().writeField(this.buffer(), key, value);
+		return get(key);
+	}
+
+	@Override
+	public Object remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends Object> m) {
+		for (Entry<? extends String, ? extends Object> e : m.entrySet())
+			this.put(e.getKey(), e.getValue());
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<String> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Object> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<String, Object>> entrySet() {
 		// TODO Auto-generated method stub
 		return null;
 	}
