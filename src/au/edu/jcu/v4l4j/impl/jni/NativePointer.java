8c2793e5dca2380bf0ebe9d93d54d27898fac304
mailmindlin <liamttlyrox@gmail.com>
2017-02-23 11:54:44 -0500

    Add NativePointer for base native memory access ops
    Pretty similar to ByteBuffer

---
+++ src/au/edu/jcu/v4l4j/impl/jni/NativePointer.java
@@ -0,0 +1,67 @@
+package au.edu.jcu.v4l4j.impl.jni;
+
+import java.nio.ByteBuffer;
+
+public NativePointer<T> implements AutoCloseable {
+	protected final long address;
+	protected final StructFieldType type;
+	protected ByteBuffer buffer;
+	
+	/**
+	 * Set of objects that we have to release when closing
+	 */
+	protected final List<AutoCloseable> managedRefs = new ArrayList<>();
+	
+	protected NativePointer(StructFieldType type, long address, ByteBuffer buffer, boolean freeOnClose) {
+		this.type = type;
+		this.address = address;
+		this.buffer = buffer;
+		if (freeOnClose)
+			managedRefs.add(this);
+	}
+	
+	public T get() {
+		return this.type().read(this.buffer(), null);
+	}
+	
+	public void set(T value) {
+		this.type().write(this.buffer(), value);
+	}
+	
+	public <U> NativePointer<U> dereference() {
+		StructFieldType type = this.type();
+		if (!(type instanceof PointerStructFieldType))
+			throw new UnsupportedOperationException("Cannot dereference non-pointer");
+		long farAddr = ((Number)type.read(buffer, null)).longValue();
+		if (farAddr == 0)
+			return null;
+		return new NativePointer(((PointerStructFieldType)type).getFarType(), farAddr, MemoryUtils.wrap(farAddr), false);
+	}
+	
+	public StructFieldType type() {
+		return type;
+	}
+	
+	public long address() {
+		return this.address;
+	}
+	
+	public ByteBuffer buffer() {
+		return this.buffer;
+	}
+	
+	@Override
+	public void close() {
+		for (AutoCloseable ref : this.managedRefs) {
+			try {
+				if (ref == this)
+					MemoryUtils.free(this.address());
+				else
+					ref.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		this.nativeRefs.clear();
+	}
+}
