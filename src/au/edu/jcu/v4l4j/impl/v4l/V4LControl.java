37c9494ddfa16ce81b4598210bc3660d482b70c7
mailmindlin <liamttlyrox@gmail.com>
2017-02-27 16:33:21 -0500

    Pretty much finish V4LControl's
    All that needs to happen is JNI bindings, but that's just copy/pasting
    at this point!

--- src/au/edu/jcu/v4l4j/impl/v4l/V4LControl.java
+++ src/au/edu/jcu/v4l4j/impl/v4l/V4LControl.java
@@ -1,25 +1,33 @@
 package au.edu.jcu.v4l4j.impl.v4l;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
+import au.edu.jcu.v4l4j.api.control.ContinuousControl;
 import au.edu.jcu.v4l4j.api.control.Control;
 import au.edu.jcu.v4l4j.api.control.ControlType;
+import au.edu.jcu.v4l4j.api.control.DiscreteControl;
 
 public abstract class V4LControl<V> implements Control<V> {
 	private static native int doGetValueInt(long pointer, int ctrlId);
 	
-	private static native int doGetValueString(long pointer, int ctrlId);
+	private static native String doGetValueString(long pointer, int ctrlId);
 	
-	private static native int doGetValueLong(long pointer, int ctrlId);
+	private static native long doGetValueLong(long pointer, int ctrlId);
 	
-	private static native int doSetValueInt(long pointer, int ctrlId, int value);
+	private static native void doSetValueInt(long pointer, int ctrlId, int value);
 	
-	private static native int doSetValueString(long pointer, int ctrlId, int value);
+	private static native void doSetValueString(long pointer, int ctrlId, String value);
 	
-	private static native int doSetValueLong(long pointer, int ctrlId, int value);
+	private static native void doSetValueLong(long pointer, int ctrlId, long value);
 	
 	protected final VideoDevice device;
 	protected final int ctrlId;
@@ -40,6 +48,30 @@ public abstract class V4LControl<V> implements Control<V> {
 	public void close() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	protected int getValueInt() {
+		return doGetValueInt(this.device.pointer, this.ctrlId);
+	}
+	
+	protected String getValueString() {
+		return doGetValueString(this.device.pointer, this.ctrlId);
+	}
+	
+	protected long getValueLong() {
+		return doGetValueLong(this.device.pointer, this.ctrlId);
+	}
+	
+	protected void setValueInt(int value) {
+		doSetValueInt(this.device.pointer, this.ctrlId, value);
+	}
+	
+	protected void setValueString(String value) {
+		doSetValueString(this.device.pointer, this.ctrlId, value);
+	}
+	
+	protected void setValueLong(long value) {
+		doSetValueLong(this.device.pointer, this.ctrlId, value);
 	}
 	
 	protected static class V4LControlAccessorState {
@@ -94,6 +126,11 @@ public abstract class V4LControl<V> implements Control<V> {
 		}
 		
 		@Override
+		public <E> ControlAccessor<P, T, E> get(BiFunction<T, R, E> merger) {
+			return andThen(state->state.setResult(merger.apply(state.getValue(), state.getResult())));
+		}
+
+		@Override
 		public V4LControlAccessor<P, T, R> set(T value) {
 			return andThen(state->state.setValue(value));
 		}
@@ -109,7 +146,9 @@ public abstract class V4LControl<V> implements Control<V> {
 		}
 		
 		@Override
-		public abstract V4LControlAccessor<P, T, R> update(BiFunction<? extends Control<T>, T, T> mappingFunction);
+		public V4LControlAccessor<P, T, R> update(BiFunction<Control<T>, T, T> mappingFunction) {
+			return andThen(state->state.setValue(mappingFunction.apply(getControl(), state.getValue())));
+		}
 		
 		@Override
 		public ControlAccessor<P, T, R> increase() {
@@ -122,9 +161,29 @@ public abstract class V4LControl<V> implements Control<V> {
 		}
 		
 		@Override
+		public ControlAccessor<P, T, R> read() {
+			return andThen(this::doWrite);
+		}
+
+		@Override
+		public ControlAccessor<P, T, R> write() {
+			return andThen(this::doRead);
+		}
+
+		@Override
+		public <C extends ControlAccessor<ControlAccessor<P, T, R>, T, R>> C thenIf(Predicate<ControlAccessor<P, T, R>> condition) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+		@Override
 		public P and() throws IllegalStateException {
 			throw new UnsupportedOperationException("Not implemented (yet?)");
 		}
+		
+		protected abstract void doWrite(V4LControlAccessorState state);
+		
+		protected abstract void doRead(V4LControlAccessorState state);
 		
 		protected void doCall(V4LControlAccessorState state) {
 			if (this.parent != null)
@@ -163,6 +222,16 @@ public abstract class V4LControl<V> implements Control<V> {
 		public ControlAccessor<Void, String, Void> access() {
 			return new V4LStringControlAccessor<>(null, null, null);
 		}
+
+		@Override
+		public boolean isDiscrete() {
+			return false;
+		}
+
+		@Override
+		public boolean isContinuous() {
+			return false;
+		}
 		
 		public class V4LStringControlAccessor<P, R> extends V4LControlAccessor<P, String, R> {
 			
@@ -177,24 +246,23 @@ public abstract class V4LControl<V> implements Control<V> {
 			}
 			
 			@Override
-			public  V4LControlAccessor<P, String, R> update(BiFunction<? extends Control<String>, String, String> mappingFunction) {
-				return andThen(state->state.setValue(mappingFunction.apply(state.getValue(), V4LControl.this)));
+			public V4LStringControl getControl() {
+				return V4LStringControl.this;
 			}
 
 			@Override
-			public ControlAccessor<P, String, R> read() {
-				return andThen(state->state.setValue(V4LControl.doGetValueString(V4LStringControl.this.device.pointer, V4LStringControl.this.ctrlId)));
+			protected void doWrite(V4LControlAccessorState state) {
+				V4LStringControl.this.setValueString(state.getValue());
 			}
 
 			@Override
-			public ControlAccessor<P, String, R> write() {
-				return andThen(state->V4LControl.doSetValueString(V4LStringControl.this.device.pointer, V4LStringControl.this.ctrlId, state.getValue()));
+			protected void doRead(V4LControlAccessorState state) {
+				state.setResult(V4LStringControl.this.getValueString());
 			}
-			
 		}
 	}
 	
-	public static class V4LSwitchControl extends V4LControl<Boolean> {
+	public static class V4LSwitchControl extends V4LControl<Boolean> implements DiscreteControl<Boolean> {
 
 		protected V4LSwitchControl(VideoDevice device, String name, int ctrlId) {
 			super(device, name, ctrlId);
@@ -207,7 +275,22 @@ public abstract class V4LControl<V> implements Control<V> {
 
 		@Override
 		public ControlAccessor<Void, Boolean, Void> access() {
-			return null;
+			return new V4LSwitchControlAccesssor<>(null, null, null);
+		}
+
+		@Override
+		public Future<Iterator<Boolean>> options() {
+			return CompletableFuture.completedFuture(Arrays.asList(true, false).iterator());
+		}
+
+		@Override
+		public boolean isDiscrete() {
+			return false;
+		}
+
+		@Override
+		public boolean isContinuous() {
+			return false;
 		}
 		
 		protected class V4LSwitchControlAccesssor<P, R> extends V4LControlAccessor<P, Boolean, R> {
@@ -215,29 +298,126 @@ public abstract class V4LControl<V> implements Control<V> {
 			protected V4LSwitchControlAccesssor(V4LControlAccessor<?, ?, ?> parent, Duration timeout, Consumer<V4LControlAccessorState> mutator) {
 				super(parent, timeout, mutator);
 			}
-
-			@Override
-			public ControlAccessor<P, Boolean, R> set() {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
+			
 			@Override
 			protected <X, Z> V4LControl<Boolean>.V4LControlAccessor<X, Boolean, Z> chained(V4LControlAccessor<?, ?, ?> parent, Duration timeout,  Consumer<V4LControlAccessorState> mutator) {
 				return new V4LSwitchControlAccesssor<>(parent, timeout, mutator);
 			}
 
 			@Override
-			public ControlAccessor<P, Boolean, Boolean> get() {
-				return andThen(state->{
-					//TODO finish
-					Object _value = state.getValue();
-					//TODO map to boolean
-					V4LControl.doSetValueInt(V4LSwitchControl.this.device.pointer, V4LSwitchControl.this.ctrlId, -1);
-				});
+			public Control<Boolean> getControl() {
+				return V4LSwitchControl.this;
 			}
 			
+			@Override
+			public ControlAccessor<P, Boolean, R> increase() {
+				return set(true);
+			}
+			
+			@Override
+			public ControlAccessor<P, Boolean, R> decrease() {
+				return set(false);
+			}
+
+			@Override
+			protected void doWrite(V4LControlAccessorState state) {
+				Object _value = state.getValue();
+				boolean bValue;
+				try {
+					bValue = (Boolean) _value;
+				} catch (NullPointerException | ClassCastException e) {
+					bValue = Boolean.parseBoolean("" + _value);
+				}
+				V4LSwitchControl.this.setValueInt(bValue ? 1 : 0);
+			}
+
+			@Override
+			protected void doRead(V4LControlAccessorState state) {
+				int iValue = V4LSwitchControl.this.getValueInt();
+				state.setValue(iValue != 0);
+			}
+		}
+	}
+	
+	public static abstract class V4LSliderControl extends V4LControl<Number> implements ContinuousControl<Number> {
+		protected final Number minValue;
+		protected final Number maxValue;
+		protected final Number step;
+		
+		protected V4LSliderControl(VideoDevice device, String name, int ctrlId, Number min, Number max, Number step) {
+			super(device, name, ctrlId);
+			this.minValue = min;
+			this.maxValue = max;
+			this.step = step;
+		}
+
+		@Override
+		public ControlType getType() {
+			return ControlType.SLIDER;
+		}
+
+		@Override
+		public ControlAccessor<Void, Number, Void> access() {
+			return new V4LSliderControlAccessor<>(null, null, null);
+		}
+
+		@Override
+		public Future<ContinuousRange<Number>> getRange() {
+			return CompletableFuture.completedFuture(new ContinuousRange<Number>() {
+				@Override
+				public Number minimum() {
+					return minValue;
+				}
+
+				@Override
+				public Number step() {
+					return step;
+				}
+
+				@Override
+				public Number defaultValue() {
+					return 0;
+				}
+
+				@Override
+				public Number maximum() {
+					return maxValue;
+				}
+				
+			});
 		}
 		
+		protected class V4LSliderControlAccessor<P, R> extends V4LControlAccessor<P, Number, R> {
+
+			protected V4LSliderControlAccessor(V4LControlAccessor<?, ?, ?> parent, Duration timeout, Consumer<V4LControlAccessorState> mutator) {
+				super(parent, timeout, mutator);
+			}
+
+			@Override
+			public Control<Number> getControl() {
+				return V4LSliderControl.this;
+			}
+
+			@Override
+			protected void doRead(V4LControlAccessorState state) {
+				state.setValue(V4LSliderControl.this.getValueLong());
+			}
+			
+			protected void doWrite(V4LControlAccessorState state) {
+				Object value = state.getValue();
+				long lValue;
+				if (value == null)
+					lValue = 0;
+				else
+					lValue = ((Number)value).longValue();
+				V4LSliderControl.this.setValueLong(lValue);
+			}
+
+			@Override
+			protected <X, Z> V4LControlAccessor<X, Number, Z> chained(V4LControl<Number>.V4LControlAccessor<?, ?, ?> parent, Duration timeout,
+					Consumer<V4LControlAccessorState> mutator) {
+				return new V4LSliderControlAccessor<>(parent, timeout, mutator);
+			}
+		}
 	}
 }
