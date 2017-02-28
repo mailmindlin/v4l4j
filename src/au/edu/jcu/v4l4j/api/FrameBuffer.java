0df9e3ffca580443e4bc8a4709bd7caabad26be7
mailmindlin <liamttlyrox@gmail.com>
2017-02-28 13:55:11 -0500

    Add FrameBuffer sequence number

--- src/au/edu/jcu/v4l4j/api/FrameBuffer.java
+++ src/au/edu/jcu/v4l4j/api/FrameBuffer.java
@@ -5,6 +5,21 @@ import java.nio.ByteBuffer;
 public interface FrameBuffer {
 	ByteBuffer asByteBuffer();
 	
+	/**
+	 * A number that increments with the buffers, indicating their order.
+	 * 
+	 * Note that the difference between the value returned from this method and
+	 * {@link #getTimestamp()} is that the sequence number is relative to other
+	 * buffers from the same port, while the timestamp refers to when the data
+	 * <i>contained</i> in the buffers was collected.
+	 * @return the sequence number for this buffer
+	 */
+	long getSequenceNumber();
+	
+	/**
+	 * Get the timestamp attached to this buffer.
+	 * @see #getSequenceNumber
+	 */
 	long getTimestamp();
 	
 	default int getCapacity() {
--- src/au/edu/jcu/v4l4j/impl/omx/OMXFrameBuffer.java
+++ src/au/edu/jcu/v4l4j/impl/omx/OMXFrameBuffer.java
@@ -7,39 +7,51 @@ import au.edu.jcu.v4l4j.api.FrameBuffer;
 public class OMXFrameBuffer implements FrameBuffer {
 	protected final long pointer;
 	private final ByteBuffer buffer;
+	private int ticks;
+	private long timestamp;
+	private int flags;
 	
 	protected OMXFrameBuffer(long pointer, ByteBuffer buffer) {
 		this.pointer = pointer;
 		this.buffer = buffer;
 	}
 	
+	protected void prepare(int ticks, long timestamp, int offset, int filled, int flags) {
+		this.ticks = ticks;
+		this.timestamp = timestamp;
+		this.buffer.position(offset);
+		this.buffer.limit(filled);
+		this.flags = flags;
+	}
+	
 	@Override
 	public ByteBuffer asByteBuffer() {
-		return this.buffer;
+		return this.buffer.duplicate();
+	}
+	
+	@Override
+	public long getSequenceNumber() {
+		return this.ticks;
 	}
 	
 	@Override
 	public long getTimestamp() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.timestamp;
 	}
 	
 	@Override
 	public int getCapacity() {
-		// TODO Auto-generated method stub
-		return 0;
+		return buffer.capacity();
 	}
 	
 	@Override
 	public int getFilled() {
-		// TODO Auto-generated method stub
-		return 0;
+		return buffer.remaining();
 	}
 	
 	@Override
 	public int getOffset() {
-		// TODO Auto-generated method stub
-		return 0;
+		return buffer.position();
 	}
 	
 	@Override
