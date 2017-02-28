e3df7278ddd300c0f44367a06fac90642696f286
mailmindlin <liamttlyrox@gmail.com>
2017-02-28 13:55:00 -0500

    Change IV port inheritance

--- src/au/edu/jcu/v4l4j/api/component/port/ImagePort.java
+++ src/au/edu/jcu/v4l4j/api/component/port/ImagePort.java
@@ -1,12 +1,8 @@
 package au.edu.jcu.v4l4j.api.component.port;
 
 import au.edu.jcu.v4l4j.api.ImageCodingType;
-import au.edu.jcu.v4l4j.api.component.ComponentPort;
 
-public interface ImagePort extends ComponentPort {
-	int getFrameWidth();
-	int getFrameHeight();
-	int getStride();
-	int getSliceHeight();
+public interface ImagePort extends IVPort {
+	
 	ImageCodingType getCodingType();
 }
--- src/au/edu/jcu/v4l4j/api/component/port/VideoPort.java
+++ src/au/edu/jcu/v4l4j/api/component/port/VideoPort.java
@@ -1,17 +1,8 @@
 package au.edu.jcu.v4l4j.api.component.port;
 
 import au.edu.jcu.v4l4j.api.VideoCompressionType;
-import au.edu.jcu.v4l4j.api.component.ComponentPort;
 
-public interface VideoPort extends ComponentPort {
-	
-	int getFrameWidth();
-	
-	int getFrameHeight();
-	
-	int getStride();
-	
-	int getSliceHeight();
+public interface VideoPort extends IVPort {
 	
 	VideoCompressionType getCompression();
 }
