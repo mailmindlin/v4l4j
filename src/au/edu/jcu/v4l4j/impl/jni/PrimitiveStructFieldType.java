33157315f8b7f307565b5c1ccce5cfd12abe1841
mailmindlin <liamttlyrox@gmail.com>
2017-03-23 15:02:22 -0400

    Support more primitive type I/O ops

--- src/au/edu/jcu/v4l4j/impl/jni/PrimitiveStructFieldType.java
+++ src/au/edu/jcu/v4l4j/impl/jni/PrimitiveStructFieldType.java
@@ -1,5 +1,6 @@
 package au.edu.jcu.v4l4j.impl.jni;
 
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
@@ -32,56 +33,70 @@ public enum PrimitiveStructFieldType implements StructFieldType {
 			} else {
 				throw new IllegalArgumentException("Cannot map to char: " + value);
 			}
-			writeFWInt(buffer, this.getSize(), cValue);
+			writeFWInt(buffer, (short) cValue);
 		}
 		
 		@Override
 		public Object read(ByteBuffer buffer, StructReadingContext context) {
-			return (char) readFWInt(buffer, this.getSize());
+			return (char) readFWInt(buffer).shortValue();
 		}
 	},
-	SHORT {
+	SHORT,
+	INT,
+	LONG,
+	LONG_LONG,
+	FLOAT {
 		@Override
 		public void write(ByteBuffer buffer, Object value) {
-			writeFWInt(buffer, this.getSize(), nValue(value).longValue());
+			writeFWFloat(buffer, nValue(value));
 		}
 		
 		@Override
 		public Object read(ByteBuffer buffer, StructReadingContext context) {
-			return readFWInt(buffer, this.getSize());
+			return readFWFloat(buffer);
 		}
 	},
-	INT(null, null),
-	LONG(null, null),
-	LONG_LONG(null, null),
-	FLOAT(null, null),
-	DOUBLE(null, null),
-	LONG_DOUBLE(null, null),
-	RAW_POINTER(
-			(buffer, value) -> {
-				if (PrimitiveStructFieldType.values()[7].getSize() == 4)
-					buffer.putInt(((Number)value).intValue());
-				else
-					buffer.putLong(((Number)value).longValue());
-			},
-			(buffer, context) -> {
-				if (PrimitiveStructFieldType.values()[7].getSize() == 4)
-					return (long) buffer.getInt();
-				else
-					return buffer.getLong();
-			}),
-	INT8(
-			(buffer, value) -> buffer.put(((Number)value).byteValue()),
-			(buffer, context) -> ((Number) buffer.get())),
-	INT16(
-			(buffer, value) -> buffer.putShort(((Number)value).shortValue()),
-			(buffer, context) -> buffer.getShort()),
-	INT32(
-			(buffer, value) -> buffer.putInt(((Number)value).intValue()),
-			(buffer, context) -> buffer.getInt()),
-	INT64(
-			(buffer, value) -> buffer.putLong(((Number)value).longValue()),
-			(buffer, context) -> buffer.getLong()),
+	DOUBLE {
+		@Override
+		public void write(ByteBuffer buffer, Object value) {
+			writeFWFloat(buffer, nValue(value));
+		}
+		
+		@Override
+		public Object read(ByteBuffer buffer, StructReadingContext context) {
+			return readFWFloat(buffer);
+		}
+	},
+	LONG_DOUBLE {
+		@Override
+		public void write(ByteBuffer buffer, Object value) {
+			writeFWFloat(buffer, nValue(value));
+		}
+		
+		@Override
+		public Object read(ByteBuffer buffer, StructReadingContext context) {
+			return readFWFloat(buffer);
+		}
+	},
+	RAW_POINTER {
+		@Override
+		public void write(ByteBuffer buffer, Object value) {
+			long lValue;
+			if (value instanceof Buffer) {
+				if (!((Buffer)value).isDirect())
+					throw new IllegalArgumentException("Cannot get pointer to non-direct buffer");
+				lValue = MemoryUtils.unwrap((ByteBuffer) value);
+			} else if (value instanceof NativePointer)
+				lValue = ((NativePointer<?>) value).address();
+			else
+				lValue = nValue(value).longValue();
+			writeFWInt(buffer, lValue);
+		}
+	},
+	INT8,
+	INT16,
+	INT32,
+	INT64,
 	FLOAT32(
 			(buffer, value) -> buffer.putFloat(((Number)value).floatValue()),
 			(buffer, context) -> buffer.getFloat()),
@@ -94,8 +109,31 @@ public enum PrimitiveStructFieldType implements StructFieldType {
 	private final BiConsumer<ByteBuffer, Object> writer;
 	private final BiFunction<ByteBuffer, StructReadingContext, Object> reader;
 	
-	private static long readFWInt(ByteBuffer buffer, int size) {
-		switch (size) {
+	private static Number nValue(Object value) {
+		if (value == null)
+			return 0;
+		if (value instanceof Number)
+			return (Number) value;
+		//TODO support BigInteger/BigDecimal?
+		throw new IllegalArgumentException("Cannot map to number: " + value);
+	}
+	
+	PrimitiveStructFieldType() {
+		this.alignment = MemoryUtils.getAlignment(this.ordinal());
+		this.size = MemoryUtils.getSize(this.ordinal());
+		this.writer = (buffer, value) -> writeFWInt(buffer, nValue(value));
+		this.reader = (buffer, ctx) -> readFWInt(buffer);
+	}
+	
+	PrimitiveStructFieldType(BiConsumer<ByteBuffer, Object> writer, BiFunction<ByteBuffer, StructReadingContext, Object> reader) {
+		this.writer = writer;
+		this.reader = reader;
+		this.alignment = MemoryUtils.getAlignment(this.ordinal());
+		this.size = MemoryUtils.getSize(this.ordinal());
+	}
+	
+	Number readFWInt(ByteBuffer buffer) {
+		switch (getSize()) {
 			case 1:
 				return buffer.get();
 			case 2:
@@ -109,44 +147,44 @@ public enum PrimitiveStructFieldType implements StructFieldType {
 		}
 	}
 	
-	private static void writeFWInt(ByteBuffer buffer, int size, long value) {
-		switch (size) {
+	Number readFWFloat(ByteBuffer buffer) {
+		switch (getSize()) {
+			case 4:
+				return buffer.getFloat();
+			case 8:
+				return buffer.getDouble();
+			default:
+				throw new UnsupportedOperationException("Cannot read " + getSize() + "-byte-long float");
+		}
+	}
+	
+	void writeFWInt(ByteBuffer buffer, Number value) {
+		switch (getSize()) {
 			case 1:
-				buffer.put((byte) value);
+				buffer.put(value.byteValue());
 				break;
 			case 2:
-				buffer.putShort((short) value);
+				buffer.putShort(value.shortValue());
 				break;
 			case 4:
-				buffer.putInt((int) value);
+				buffer.putInt(value.intValue());
 				break;
 			case 8:
-				buffer.putLong((long) value);
+				buffer.putLong(value.longValue());
 			default:
 				throw new IllegalArgumentException();
 		}
 	}
 	
-	private static Number nValue(Object value) {
-		if (value == null)
-			return 0;
-		if (value instanceof Number)
-			return (Number) value;
-		throw new IllegalArgumentException("Cannot map to number: " + value);
-	}
-	
-	PrimitiveStructFieldType() {
-		this.alignment = MemoryUtils.getAlignment(this.ordinal());
-		this.size = MemoryUtils.getSize(this.ordinal());
-		this.reader = null;
-		this.writer = null;
-	}
-	
-	PrimitiveStructFieldType(BiConsumer<ByteBuffer, Object> writer, BiFunction<ByteBuffer, StructReadingContext, Object> reader) {
-		this.writer = writer;
-		this.reader = reader;
-		this.alignment = MemoryUtils.getAlignment(this.ordinal());
-		this.size = MemoryUtils.getSize(this.ordinal());
+	void writeFWFloat(ByteBuffer buffer, Number value) {
+		switch (getSize()) {
+			case 4:
+				buffer.putFloat(value.floatValue());
+			case 8:
+				buffer.putDouble(value.doubleValue());
+			default:
+				throw new UnsupportedOperationException("Cannot write " + getSize() + "-byte-long float");
+		}
 	}
 	
 	@Override
