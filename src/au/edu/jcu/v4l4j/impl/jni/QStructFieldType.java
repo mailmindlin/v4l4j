d66582d111bd371f5366ab781e32d8d7004a5b68
mailmindlin <liamttlyrox@gmail.com>
2017-03-23 11:53:53 -0400

    Generalize Q16StructFieldType
    Not sure that any others will be used, but it can't hurt...

---
+++ src/au/edu/jcu/v4l4j/impl/jni/QStructFieldType.java
@@ -0,0 +1,55 @@
+package au.edu.jcu.v4l4j.impl.jni;
+
+import java.io.ObjectStreamException;
+import java.nio.ByteBuffer;
+
+public class QStructFieldType implements StructFieldType {
+	private static final long serialVersionUID = -4262354267638314597L;
+	
+	public static final QStructFieldType Q16 = new QStructFieldType(16);
+	
+	public static QStructFieldType of(int n) {
+		if (n == 16)
+			return Q16;
+		return new QStructFieldType(n);
+	}
+	
+	private int n;
+	
+	protected QStructFieldType(int n) {
+		this.n = n;
+	}
+	
+	@Override
+	public int getAlignment() {
+		return PrimitiveStructFieldType.INT32.getAlignment();
+	}
+	
+	@Override
+	public int getSize() {
+		return PrimitiveStructFieldType.INT32.getSize();
+	}
+
+	@Override
+	public boolean expands() {
+		return false;
+	}
+	
+	@Override
+	public void write(ByteBuffer buffer, Object params) {
+		int iValue = (int) Math.scalb(((Number)params).floatValue(), - this.n);
+		PrimitiveStructFieldType.INT32.write(buffer, iValue);
+	}
+	
+	@Override
+	public Object read(ByteBuffer buffer, StructReadingContext context) {
+		float fValue = ((Number) PrimitiveStructFieldType.INT32.read(buffer, context)).floatValue();
+		return Math.scalb(fValue, this.n);
+	}
+	
+	private Object readResolve() throws ObjectStreamException {
+		if (this.n == 16)
+			return QStructFieldType.Q16;
+		return this;
+	}
+}
