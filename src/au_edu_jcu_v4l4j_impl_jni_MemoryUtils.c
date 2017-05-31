88ba248945ee62b17ab10693b2618d3b92b49cac
mailmindlin <liamttlyrox@gmail.com>
2017-05-31 18:59:26 -0400

    Cleaned up methods to get compile-machine native info
    Macros + arrays to our rescue.

--- src/au_edu_jcu_v4l4j_impl_jni_MemoryUtils.c
+++ src/au_edu_jcu_v4l4j_impl_jni_MemoryUtils.c
@@ -10,6 +10,7 @@
 #include <stdbool.h>
 #include <stdlib.h>//aligned_alloc, free
 #include <string.h>//memset
+#include <stdalign.h>//alignof
 
 #include "common.h"
 #include "debug.h"
@@ -21,175 +22,64 @@
 extern "C" {
 #endif
 
-
-struct BoolAlignmentStruct {
-	uint8_t padding;
-	bool test;
-};
-
-struct CharAlignmentStruct {
-	uint8_t padding;
-	char test;
-};
-
-struct ShortAlignmentStruct {
-	uint8_t padding;
-	short test;
-};
-
-struct IntAlignmentStruct {
-	uint8_t padding;
-	int test;
-};
-
-struct LongAlignmentStruct {
-	uint8_t padding;
-	long test;
-};
-
-struct LongLongAlignmentStruct {
-	uint8_t padding;
-	long long test;
-};
-
-struct FloatAlignmentStruct {
-	uint8_t padding;
-	float test;
-};
-
-struct DoubleAlignmentStruct {
-	uint8_t padding;
-	double test;
-};
-
-struct LongDoubleAlignmentStruct {
-	uint8_t padding;
-	long double test;
-};
-
-struct PointerAlignmentStruct {
-	uint8_t padding;
-	void* test;
-};
-
-struct Int8AlignmentStruct {
-	uint8_t padding;
-	uint8_t test;
-};
-
-struct Int16AlignmentStruct {
-	uint8_t padding;
-	uint16_t test;
-};
-
-struct Int32AlignmentStruct {
-	uint8_t padding;
-	uint32_t test;
-};
-
-struct Int64AlignmentStruct {
-	uint8_t padding;
-	uint64_t test;
-};
-
-struct F32AlignmentStruct {
-	uint8_t padding;
-	float test;
-};
-
-struct F64AlignmentStruct {
-	uint8_t padding;
-	double test;
-};
-
-JNIEXPORT jint JNICALL Java_au_edu_jcu_v4l4j_impl_jni_MemoryUtils_getAlignment(JNIEnv *env, jclass me, jint type) {
-	LOG_FN_ENTER();
-	
-	switch (type) {
-		case 0://Bool
-			return offsetof(struct BoolAlignmentStruct, test);
-		case 1://char
-			return offsetof(struct CharAlignmentStruct, test);
-		case 2://short
-			return offsetof(struct ShortAlignmentStruct, test);
-		case 3://int
-			return offsetof(struct IntAlignmentStruct, test);
-		case 4://long
-			return offsetof(struct LongAlignmentStruct, test);
-		case 5://long long
-			return offsetof(struct LongAlignmentStruct, test);
-		case 6://float
-			return offsetof(struct FloatAlignmentStruct, test);
-		case 7://double
-			return offsetof(struct DoubleAlignmentStruct, test);
-		case 8://long double
-			return offsetof(struct LongDoubleAlignmentStruct, test);
-		case 9://pointer (void*)
-			return offsetof(struct PointerAlignmentStruct, test);
-		case 10://int8
-			return offsetof(struct Int8AlignmentStruct, test);
-		case 11://int16
-			return offsetof(struct Int16AlignmentStruct, test);
-		case 12://int32
-			return offsetof(struct Int32AlignmentStruct, test);
-		case 13://int64
-			return offsetof(struct Int64AlignmentStruct, test);
-		case 14://float32
-			return offsetof(struct Float32AlignmentStruct, test);
-		case 15://float64
-			return offsetof(struct Float64AlignmentStruct, test);
-		default:
-			return -1;
-	}	
-}
-
+#define INFO(type) {alignof(type), sizeof(type)}
 
 struct EmptyStruct {};
 union EmptyUnion {};
 
+/*
+ * Info about c datatypes.
+ * Must reflect PrimitiveStructFieldType.
+ */
+static int const PRIMITIVE_INFO[][2] = {
+	INFO(bool),
+	INFO(char),
+	INFO(signed char),
+	INFO(unsigned char),
+	INFO(short),
+	INFO(unsigned short),
+	INFO(int),
+	INFO(unsigned int),
+	INFO(long),
+	INFO(unsigned long),
+	INFO(long long),
+	INFO(unsigned long long),
+	INFO(float),
+	INFO(double),
+	INFO(long double),
+	INFO(void*),
+	INFO(int8_t),
+	INFO(uint8_t),
+	INFO(int16_t),
+	INFO(uint16_t),
+	INFO(int32_t),
+	INFO(uint32_t),
+	INFO(int64_t),
+	INFO(uint64_t),
+	INFO(float),
+	INFO(double),
+	INFO(struct EmptyStruct),
+	INFO(union EmptyUnion)
+};
+
+#undef INFO
+
+JNIEXPORT jint JNICALL Java_au_edu_jcu_v4l4j_impl_jni_MemoryUtils_getAlignment(JNIEnv *env, jclass me, jint type) {
+	LOG_FN_ENTER();
+	
+	if (type > (sizeof(PRIMITIVE_INFO) / sizeof(PRIMITIVE_INFO[0])))
+		return -1;
+	
+	return PRIMITIVE_INFO[type][0];
+}
+
 JNIEXPORT jint JNICALL Java_au_edu_jcu_v4l4j_impl_jni_MemoryUtils_getSize(JNIEnv *env, jclass me, jint type) {
 	LOG_FN_ENTER();
 	
-	switch (type) {
-		case 0://Bool
-			return sizeof(bool);
-		case 1://char
-			return sizeof(char);
-		case 2://short
-			return sizeof(short);
-		case 3://int
-			return sizeof(int);
-		case 4://long
-			return sizeof(long);
-		case 5://long long
-			return sizeof(long);
-		case 6://float
-			return sizeof(float);
-		case 7://double
-			return sizeof(double);
-		case 8://long double
-			return sizeof(long double);
-		case 9://pointer (void*)
-			return sizeof(void*);
-		case 10://int8
-			return sizeof(uint8_t);
-		case 11://int16
-			return sizeof(uint16_t);
-		case 12://int32
-			return sizeof(uint32_t);
-		case 13://int64
-			return sizeof(uint64_t);
-		case 14://float32
-			return sizeof(float);
-		case 15://float64
-			return sizeof(double);
-		case 16://struct
-			return sizeof(struct EmptyStruct);
-		case 17://union
-			return sizeof(union EmptyUnion);
-		default:
-			return -1;
-	}
+	if (type > (sizeof(PRIMITIVE_INFO) / sizeof(PRIMITIVE_INFO[0])))
+		return -1;
+	
+	return PRIMITIVE_INFO[type][1];
 }
 
 
