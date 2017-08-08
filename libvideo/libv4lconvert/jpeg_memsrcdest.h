f933f2986de4880967e03ea06cfe27722a31303f
mailmindlin <mailmindlin@users.noreply.github.com>
2017-08-07 22:28:04 -0400

    Fix jpeg_mem_src signature

--- libvideo/libv4lconvert/jpeg_memsrcdest.h
+++ libvideo/libv4lconvert/jpeg_memsrcdest.h
@@ -1,6 +1,6 @@
 #include <jpeglib.h>
 #include "../types.h"
 
-void jpeg_mem_src (j_decompress_ptr cinfo, u8 * buffer, unsigned long bufsize);
+void jpeg_mem_src (j_decompress_ptr cinfo, const u8 * buffer, unsigned long bufsize);
 
 void jpeg_mem_dest (j_compress_ptr cinfo, u8 ** outbuffer, unsigned long * outsize);
