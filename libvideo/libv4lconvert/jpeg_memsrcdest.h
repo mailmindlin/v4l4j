12c5bab44cfb864bc7ee185d0d73851571ed9d88
mailmindlin <mailmindlin@users.noreply.github.com>
2017-08-28 14:42:40 -0400

    Fix compatibility with libjpeg-turbo

--- libvideo/libv4lconvert/jpeg_memsrcdest.h
+++ libvideo/libv4lconvert/jpeg_memsrcdest.h
@@ -1,6 +1,10 @@
 #include <jpeglib.h>
 #include "../types.h"
 
+#if JPEG_LIB_VERSION < 80 && !defined(MEM_SRCDEST_SUPPORTED)
+
 void jpeg_mem_src (j_decompress_ptr cinfo, const u8 * buffer, unsigned long bufsize);
 
 void jpeg_mem_dest (j_compress_ptr cinfo, u8 ** outbuffer, unsigned long * outsize);
+
+#endif
